46daa8b3aed1a4f2df0ac2116088e370ffe41e39
digijin <james@digijin.com>
2017-07-14 13:24:20 +1000

    moving all firing into player

--- src/Game.js
+++ src/Game.js
@@ -48,56 +48,35 @@ export default class Game{
 
         let engine = new Engine({ctx:this.ctx});
         let firing = false;
-        engine.register({
-            update: ({ctx, mouse}) => {
+        // engine.register({
+        //     update: ({ctx, mouse}) => {
                 
-                // console.log('update');
-                if(firing){
-                    engine.register(new Shell({
-                        x: player.position.x,
-                        y: player.position.y,
-                        h: Math.random()-0.5,
-                        v: -Math.random()
-                    }))
-                    let diff = mouse.position.subtract(player.position);
-                    let dir = Math.atan2(diff.y, diff.x)
-                    dir += (Math.random()-0.5)/10 //spread
-                    engine.register(new Bullet({
-                        x: player.position.x,
-                        y: player.position.y,
-                        // h: 10+Math.random(),
-                        // v: (Math.random()-0.5)/3
-                        h:Math.cos(dir)*10,
-                        v:Math.sin(dir)*10
-                    }))
+        //         // console.log('update');
+        //         if(firing){
+        //             engine.register(new Shell({
+        //                 x: player.position.x,
+        //                 y: player.position.y,
+        //                 h: Math.random()-0.5,
+        //                 v: -Math.random()
+        //             }))
+        //             let diff = mouse.position.subtract(player.position);
+        //             let dir = Math.atan2(diff.y, diff.x)
+        //             dir += (Math.random()-0.5)/10 //spread
+        //             engine.register(new Bullet({
+        //                 x: player.position.x,
+        //                 y: player.position.y,
+        //                 // h: 10+Math.random(),
+        //                 // v: (Math.random()-0.5)/3
+        //                 h:Math.cos(dir)*10,
+        //                 v:Math.sin(dir)*10
+        //             }))
 
-                }
+        //         }
                 
-            }
-        })
+        //     }
+        // })
         engine.register(player);
-        document.addEventListener("mousedown", (e) => {
-            switch(e.button){
-                case 0:
-                    firing = true;
-                break;
-                case 2: 
-                    engine.register(new Missile({
-                        direction: -Math.PI/2,
-                        speed: 3,
-                        position: player.position.clone(),
-                        target: new Point({x:e.clientX, y: e.clientY})
-                    }));
-                break;
-            }
-        })
-        document.addEventListener("mouseup", (e) => {
-            switch(e.button){
-                case 0:
-                    firing = false;
-                break;
-            }
-        })
+        
 
 
         engine.update();  //starts
--- src/Player.js
+++ src/Player.js
@@ -1,19 +1,72 @@
 const hSpeed = 150;
 
+import Point from 'Point'
+import Missile from 'Missile';
+import Bullet from 'Bullet';
+import Shell from 'Shell';
 import mech from './mech.png'
+let firing = false;
+let missile = false
+
+document.addEventListener("mousedown", (e) => {
+    switch(e.button){
+        case 0:
+            firing = true;
+        break;
+        case 2: 
+            missile = true;
+        break;
+    }
+})
+document.addEventListener("mouseup", (e) => {
+    switch(e.button){
+        case 0:
+            firing = false;
+        break;
+    }
+})
 
 export default class Player{
     position: Point;
     constructor(params){
         Object.assign(this, params);
     }
-    update({ctx, keyboard, deltaTime}){
+    update({ctx, mouse, keyboard, deltaTime, register}){
         /* 
         w 87
         a 65
         s 83
         d 68
         */
+        if(missile){
+            missile = false;
+            register(new Missile({
+                direction: -Math.PI/2,
+                speed: 3,
+                position: this.position.clone(),
+                target: mouse.position.clone()
+            }));
+        }
+        if(firing){
+            register(new Shell({
+                x: this.position.x,
+                y: this.position.y,
+                h: Math.random()-0.5,
+                v: -Math.random()
+            }))
+            let diff = mouse.position.subtract(this.position);
+            let dir = Math.atan2(diff.y, diff.x)
+            dir += (Math.random()-0.5)/10 //spread
+            register(new Bullet({
+                x: this.position.x,
+                y: this.position.y,
+                // h: 10+Math.random(),
+                // v: (Math.random()-0.5)/3
+                h:Math.cos(dir)*10,
+                v:Math.sin(dir)*10
+            }))
+
+        }
 
         if(keyboard.down(65)){
             this.position.x -= deltaTime*hSpeed;
