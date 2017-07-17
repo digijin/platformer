93b24726d68b58412ee84cf30d6a0a5d76b932d6
digi <james@digijin.com>
2017-07-17 22:31:11 +1000

    pretty up engien

--- src/Engine.js
+++ src/Engine.js
@@ -2,6 +2,7 @@
 import Keyboard from 'Keyboard';
 import Missile from 'Missile';
 import Mouse from 'Mouse';
+import State from 'State';
 
 export default class Engine{
     objects:Array<Object>;
@@ -9,15 +10,20 @@ export default class Engine{
     lastTime: number;
     mouse:Mouse;
     deltaTime: number;
+    state:State
+
+    //init
     constructor({ctx}){
         this.mouse = new Mouse();
         this.keyboard = new Keyboard();
         this.ctx = ctx;
         this.objects = []
         this.lastTime = new Date().getTime();
+        this.state = new State();
     }
+
+    //add new objects to be tracked by engine
     register = (obj:Object) => {
-        // if(!this)debugger;
         obj.destroy = () => {
             let i = this.objects.indexOf(obj);
             if(i>-1){
@@ -25,26 +31,23 @@ export default class Engine{
             }
         }
         this.objects.push(obj)
-        // console.log(this.objects.length)
     }
+
+    //main game loop
     update = () => {
+        //handle time
         let nowTime = new Date().getTime();
         let diff = nowTime - this.lastTime;
         this.lastTime = nowTime;
-
         this.deltaTime = diff/1000
+
+        //clear canvas
         this.ctx.clearRect(0,0,500, 500)
 
-        this.objects.forEach(o => {o.update(
-        // {
-        //     ctx:this.ctx, 
-        //     deltaTime:diff/1000,
-        //     mouse:this.mouse,
-        //     keyboard:this.keyboard
-        // }
-        this
-        )});
+        //update all object
+        this.objects.forEach(o => {o.update(this)});
 
+        //wait for next frame
         requestAnimationFrame(this.update)
     }
 }
