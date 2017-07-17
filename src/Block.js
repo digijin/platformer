2bae20467cb525caa9102d17b9fcac1b956113b1
digi <james@digijin.com>
2017-07-17 21:52:04 +1000

    copypasta block and rect

---
+++ src/Block.js
@@ -0,0 +1,58 @@
+// @flow
+import config from 'config';
+import Point from 'Point';
+// import {makeKey} from 'Util/index';
+import Rect from 'Rect';
+
+export default class Block{
+  x:number;
+  y:number;
+  constructor(pos:{x:number, y:number}){
+    this.x = pos.x;
+    this.y = pos.y;
+    if(arguments.length>1){
+      throw new Error('your block args are fucked, bro.')
+    }
+  }
+  add(diff:{x:number, y:number}){
+    return new Block({
+      x: this.x + diff.x,
+      y: this.y + diff.y,
+    })
+  }
+  get center():Point{
+    return new Point({
+      x: (this.x+.5) * config.grid.width,
+      y: (this.y+.5) * config.grid.height
+    });
+  }
+  get point():Point{
+    return new Point({
+      x: this.x * config.grid.width,
+      y: this.y * config.grid.height
+    });
+  }
+  is(block:Block):boolean{
+    return (block.x === this.x && block.y === this.y);
+  }
+
+  get rect():Rect{
+    return new Rect({
+      t: this.y * config.grid.height,
+      r: (this.x + 1) * config.grid.width,
+      b: (this.y + 1) * config.grid.height,
+      l: this.x * config.grid.width
+    });
+  }
+
+  get key():string{
+    return makeKey(this.x, this.y);
+  }
+
+  static fromPoint(point:Point){
+    return new Block({
+      x: Math.floor(point.x / config.grid.width),
+      y: Math.floor(point.y / config.grid.height)
+    })
+  }
+}
