56c3cb6aba175b2dfd229168f734d9388898d3b6
digi <james@digijin.com>
2017-07-26 23:52:15 +1000

    tags

--- src/GameObject.js
+++ src/GameObject.js
@@ -2,4 +2,14 @@
 
 export default class GameObject {
 	destroy: Function;
+	tags: Array<string>;
+	constructor() {
+		this.tags = [];
+	}
+	tag = (tag: string) => {
+		this.tags.push(tag);
+	};
+	hasTag = tag => {
+		return this.tags.indexOf(tag) > -1;
+	};
 }
