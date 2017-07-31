9a4c82df218790f9e3ddc3277ba70fdc7e72ff0a
digi <james@digijin.com>
2017-07-31 18:50:24 +1000

    spawning

--- src/Spawner.js
+++ src/Spawner.js
@@ -2,14 +2,33 @@
 
 const maxEnemies = 4;
 
+import Enemy from "Enemy";
+
+import Point from "Point";
+
+import type Block from "Block";
+
 import GameObject from "GameObject";
 export default class Spawner extends GameObject {
 	update() {
 		// console.log("spawner", this.engine.objects.length);
 		if (this.engine.objectsTagged("enemy").length < maxEnemies) {
 			//space for more enemies;
-			let blocks = this.engine.grid.blocksFlattened();
-			// console.log("bl", blocks.length);
+			this.spawnEnemy();
+		}
+	}
+
+	spawnEnemy() {
+		let emptyBlocks: Array<Block> = this.engine.grid
+			.blocksFlattened()
+			.filter(b => b.type == "0");
+
+		if (emptyBlocks.length > 0) {
+			let block: Block =
+				emptyBlocks[Math.floor(emptyBlocks.length * Math.random())];
+			this.engine.register(new Enemy({ position: new Point(block.center) }));
+		} else {
+			throw new Error("no empty blocks");
 		}
 	}
 }
