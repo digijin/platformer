3cb2e6251dfefd3c5fe0f45430f2776f3f9f8f68
digijin <james@digijin.com>
2017-10-09 14:01:56 +0100

    gamepad to skip menu

--- src/MainMenu/Menu.js
+++ src/MainMenu/Menu.js
@@ -1,9 +1,17 @@
- export default class MainMenu{
-     constructor(){
+import GameObject from "GameObject";
 
-     }
-     update({ctx, deltaTime}){
-        ctx.rotate(deltaTime)
-        ctx.fillText('game!', 0, 6);
-     }
- }
+import Level from "Scene/Level";
+
+export default class MainMenu extends GameObject {
+	constructor() {
+		super();
+	}
+	update() {
+		if (this.engine.input.getButton("jump")) {
+			this.engine.startScene(new Level());
+		}
+		this.engine.ctx.translate(1, -1);
+		this.engine.ctx.rotate(this.engine.deltaTime);
+		this.engine.ctx.fillText("game!", 0, 6);
+	}
+}
--- src/Scene/MainMenu.js
+++ src/Scene/MainMenu.js
@@ -2,9 +2,12 @@ import Base from "./Base";
 
 import type Engine from "Engine";
 
+import Menu from "MainMenu/Menu";
+
 export default class MainMenu extends Base {
 	start(engine: Engine) {
 		super.start(engine);
 		engine.ui.dispatch({ type: "START_SCENE", scene: "menu" });
+		engine.register(new Menu());
 	}
 }
