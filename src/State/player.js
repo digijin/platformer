6a125ae2c4b46beb717b91df6351c86aa1a9ccd1
digijin <james@digijin.com>
2017-10-15 20:28:54 +1100

    remove gulp

--- src/State/player.js
+++ src/State/player.js
@@ -1,18 +1,14 @@
+export default function player(state, action) {
+	if (!state) {
+		state = {
+			money: 1000
+		};
+	}
 
-export default function player(state, action){
-
-    if(!state){
-        state = {
-            money: 1000
-        }
-    }
-
-
-    switch(action.type){
-        case 'SPEND_MONEY':
-            state.money -= action.cost
-        break;
-    }
-    return state
-
+	switch (action.type) {
+		case "SPEND_MONEY":
+			state.money -= action.cost;
+			break;
+	}
+	return state;
 }
