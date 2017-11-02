b275326cbc3a30bc840829b76a9855197729828d
digi <james@digijin.com>
2017-11-03 00:58:05 +1100

    thinking about grid tests

--- src/Grid.spec.js
+++ src/Grid.spec.js
@@ -1,11 +1,17 @@
 import Grid from "Grid";
 import config from "config";
 import Point from "Point";
+import Block from "Block";
 
 describe("Grid", () => {
 	describe("blocksInRect", () => {
 		it("should return", () => {
+			config.grid.width = 20;
+			config.grid.height = 20;
+
 			let grid = new Grid();
+
+			grid.blocks = [[new Block({ position: { x: 0, y: 0 } })], [], []];
 		});
 	});
 });
