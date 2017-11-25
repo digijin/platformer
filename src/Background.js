8b738d4898637df22c4b6cdf5ac0d60131be7a6b
digi <james@digijin.com>
2017-11-25 20:49:19 +1100

    moving styles to css

--- src/Background.js
+++ src/Background.js
@@ -15,20 +15,13 @@ export default class Background extends GameObject {
 	init(engine: Engine) {
 		super.init(engine);
 		this.el = document.createElement("DIV");
-		// this.el.id = "background";
+		this.el.id = "background";
 		engine.container.appendChild(this.el);
 
-		// this.el.style.position = "relative";
 		this.el.style.backgroundImage = url;
-		this.el.style.backgroundPosition = "bottom";
-		this.el.style.backgroundRepeat = "repeat-x";
-		this.el.style.width = "100%";
-		this.el.style.height = "275px";
-		// this.el.style.opacity = 0.5;
-		// this.el.style.border = "1px solid red";
 	}
 	update() {
 		this.el.style.backgroundPositionX =
-			-this.engine.view.offset.x / 16 + "px";
+			-this.engine.view.offset.x / 8 + "px";
 	}
 }
