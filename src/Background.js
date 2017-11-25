930883efef9feca6d55bbbfca908ac2a8b6e18f0
digi <james@digijin.com>
2017-11-25 23:50:52 +1100

    styling background

--- src/Background.js
+++ src/Background.js
@@ -22,25 +22,28 @@ export default class Background extends GameObject {
 		this.el.id = "background";
 		engine.container.appendChild(this.el);
 
-		// let layer1 = document.createElement("DIV");
-		// layer1.className = "layer1"
-		// this.el.appendChild(this.top);
+		let layer1 = document.createElement("DIV");
+		layer1.className = "layer layer1";
+		this.el.appendChild(layer1);
+		let layer2 = document.createElement("DIV");
+		layer2.className = "layer layer2";
+		this.el.appendChild(layer2);
 
 		this.top = document.createElement("DIV");
-		this.top.className = "top layer1";
-		this.el.appendChild(this.top);
+		this.top.className = "top";
+		layer1.appendChild(this.top);
 
 		this.bottom = document.createElement("DIV");
-		this.bottom.className = "bottom layer1";
-		this.el.appendChild(this.bottom);
+		this.bottom.className = "bottom";
+		layer1.appendChild(this.bottom);
 
 		this.top2 = document.createElement("DIV");
-		this.top2.className = "top layer2";
-		this.el.appendChild(this.top2);
+		this.top2.className = "top";
+		layer2.appendChild(this.top2);
 
 		this.bottom2 = document.createElement("DIV");
-		this.bottom2.className = "bottom layer2";
-		this.el.appendChild(this.bottom2);
+		this.bottom2.className = "bottom";
+		layer2.appendChild(this.bottom2);
 
 		this.top.style.backgroundImage = url;
 		this.top2.style.backgroundImage = url;
@@ -50,5 +53,10 @@ export default class Background extends GameObject {
 			-this.engine.view.offset.x / 8 + "px";
 		this.top2.style.backgroundPositionX =
 			-this.engine.view.offset.x / 4 + "px";
+
+		this.top.style.height = 250 - this.engine.view.offset.y / 4 + "px";
+		this.bottom.style.top = 250 - this.engine.view.offset.y / 4 + "px";
+		this.top2.style.height = 350 - this.engine.view.offset.y / 5 + "px";
+		this.bottom2.style.top = 350 - this.engine.view.offset.y / 5 + "px";
 	}
 }
