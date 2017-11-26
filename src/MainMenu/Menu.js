f33105c5dd30d7e29c5d09c458d2540eba8c2330
digi <james@digijin.com>
2017-11-26 21:44:00 +1100

    main menu particle effect

--- src/MainMenu/Menu.js
+++ src/MainMenu/Menu.js
@@ -18,26 +18,38 @@ export default class MainMenu extends GameObject {
 	constructor() {
 		super();
 		this.particles = [];
+		this.spawnTimer = 0;
+		this.timePassed = 0;
 	}
 	init(engine: Engine) {
 		super.init(engine);
 		engine.view.offset = new Point({ x: 0, y: 0 });
 	}
+	spawnTimer: number;
 	particles: Array<Particle>;
-	particleFX() {
+	leftright: boolean;
+	particleFX(center: Point) {
 		//spawn
-		this.particles.push({
-			time: 0,
-			position: new Point({ x: 300 * (Math.random() - 0.5), y: 0 })
-		});
-
-		let center = new Point({
-			x: window.innerWidth / 2,
-			y: window.innerHeight / 2
-		});
+		this.spawnTimer -= this.engine.deltaTime;
+		if (this.spawnTimer < 0) {
+			this.spawnTimer += 0.8;
+			let x = 30 * Math.random();
+			this.leftright = !this.leftright;
+			if (this.leftright) {
+				x = -x;
+			}
+			this.particles.push({
+				time: 0,
+				rotation: x * Math.PI / 180 * 2,
+				position: new Point({
+					x: x,
+					y: 0
+				})
+			});
+		}
 		this.particles = this.particles.filter(part => {
 			part.time += this.engine.deltaTime;
-			let lifetime = 2;
+			let lifetime = 25;
 			if (part.time > lifetime) {
 				return false;
 			}
@@ -46,28 +58,51 @@ export default class MainMenu extends GameObject {
 			let ageipc = 100 - agepc; //age inverse pc
 			this.engine.ctx.context.filter =
 				"saturate(" +
-				ageipc +
-				"%) blur(" +
-				age * 4 +
-				"px) opacity(" +
-				ageipc +
+				Math.max((1 - age * 4) * 100, 0) +
+				"%) opacity(" +
+				ageipc * 2 +
 				"%)";
+			// "saturate(" +
+			// (age - 1) * 100 +
+			// "%) blur(" +
+			// age * 2 +
+			// "px) opacity(" +
+			// ageipc * 2 +
+			// "%)";
 
-			part.position.y -= this.engine.deltaTime * 100;
+			part.position.y -= this.engine.deltaTime * 20;
+			part.position.x -= this.engine.deltaTime * (4 + Math.random());
 			this.engine.ctx.drawSprite(
 				explosion,
 				part.position.add(center),
 				{
-					w: 80,
-					h: 80
+					w: 40 * (1 - age) + 40,
+					h: 40 * (1 - age) + 40
 				},
-				-Math.PI / 2
+				part.rotation - Math.PI / 2
 			);
+			// this.engine.ctx.clearRect(
+			// 	0,
+			// 	window.innerHeight / 2,
+			// 	window.innerWidth,
+			// 	100
+			// );
 
 			return true;
 		});
 		this.engine.ctx.context.filter = "none";
 	}
+	ground() {
+		this.engine.ctx.resetTransform();
+		this.engine.ctx.context.fillStyle = "#222";
+		this.engine.ctx.context.fillRect(
+			0,
+			window.innerHeight / 2,
+			window.innerWidth,
+			window.innerHeight / 2
+		);
+	}
+	timePassed: number;
 	update() {
 		if (this.engine.input.getButton("jump")) {
 			this.engine.startScene(new Level());
@@ -77,15 +112,17 @@ export default class MainMenu extends GameObject {
 		// this.engine.ctx.fillText("game!", 0, 6);
 		this.engine.ctx.context.filter = "none";
 
-		this.engine.ctx.resetTransform();
-		this.engine.ctx.context.fillStyle = "#222";
-		this.engine.ctx.context.fillRect(
-			0,
-			window.innerHeight / 2,
-			window.innerWidth,
-			window.innerHeight / 2
+		this.particleFX(
+			new Point({
+				x: window.innerWidth / 2 + 300,
+				y: window.innerHeight / 2 + 50
+			})
 		);
-		this.particleFX();
+		this.ground();
+		this.hero();
+	}
+
+	hero() {
 		this.engine.ctx.drawSprite(
 			mechHero,
 			new Point({
