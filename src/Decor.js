f750b517e9bc8070f27cf4b5c2f9491d69d56496
digi <james@digijin.com>
2017-11-29 19:07:08 +1100

    decor

---
+++ src/Decor.js
@@ -0,0 +1,98 @@
+//@flow
+
+import config from "config";
+import Point from "Point";
+import Rect from "Rect";
+import type Grid from "Grid";
+
+import type BlockType from "BlockType";
+import { BlockTypeMap } from "BlockType";
+
+export default class Block {
+	position: Point;
+	type: string;
+	grid: Grid;
+	hp: number;
+	constructor(params: { position: Point, type: string, grid: Grid }) {
+		this.position = params.position;
+		this.type = params.type;
+		this.grid = params.grid;
+
+		if (this.type !== "0") {
+			this.hp = this.getType().hp;
+		}
+	}
+	toString() {
+		return JSON.stringify({ position: this.position, type: this.type });
+	}
+
+	getType(): BlockType {
+		return BlockTypeMap[this.type];
+	}
+
+	isEmpty(): boolean {
+		return this.type == "0";
+	}
+	damage(amount: number) {
+		let type = this.getType();
+		if (type && type.destructable) {
+			this.hp -= amount;
+			if (this.hp <= 0) {
+				this.destroy();
+			}
+		}
+	}
+	destroy() {
+		this.type = "0";
+		this.grid.bustCache(this);
+	}
+	//for editor
+	add(blockId: string) {
+		this.type = blockId;
+		this.grid.bustCache(this);
+	}
+	//for editor
+	remove() {
+		this.type = "0";
+		this.grid.bustCache(this);
+	}
+
+	get center(): Point {
+		return new Point({
+			x: (this.position.x + 0.5) * config.grid.width,
+			y: (this.position.y + 0.5) * config.grid.height
+		});
+	}
+	get point(): Point {
+		return new Point({
+			x: this.position.x * config.grid.width,
+			y: this.position.y * config.grid.height
+		});
+	}
+	is(block: Block): boolean {
+		return (
+			block.position.x === this.position.x &&
+			block.position.y === this.position.y
+		);
+	}
+
+	get rect(): Rect {
+		return new Rect({
+			t: this.position.y * config.grid.height,
+			r: (this.position.x + 1) * config.grid.width,
+			b: (this.position.y + 1) * config.grid.height,
+			l: this.position.x * config.grid.width
+		});
+	}
+
+	// get key():string{
+	//   return makeKey(this.x, this.y);
+	// }
+
+	// static fromPoint(point: Point) {
+	// 	return new Block({
+	// 		x: Math.floor(point.x / config.grid.width),
+	// 		y: Math.floor(point.y / config.grid.height)
+	// 	});
+	// }
+}
---
+++ src/DecorType.js
@@ -0,0 +1,75 @@
+//@flow
+import dirtTile from "Grid/dirt_tile.png";
+import brickTile from "Grid/brick_tile.png";
+import metalTile from "Grid/metal_tile.png";
+import woodTile from "Grid/wood_tile.png";
+
+export type BlockTypeParams = {
+	// hp: name,
+	name: string,
+	id: string,
+	image: any,
+	destructable: boolean,
+	hp: number
+};
+
+export default class BlockType {
+	name: string;
+	id: string;
+	destructable: boolean;
+	image: any;
+	hp: number;
+	constructor(params: BlockTypeParams) {
+		this.name = params.name;
+		this.id = params.id;
+		this.destructable = params.destructable;
+		this.hp = params.hp;
+		this.image = params.image;
+	}
+}
+
+let blockTypeConfig: Array<BlockTypeParams> = [
+	{
+		name: "dirt",
+		id: "1",
+		image: dirtTile,
+		destructable: true,
+		hp: 100
+	},
+	{
+		name: "brick",
+		id: "2",
+		image: brickTile,
+		destructable: true,
+		hp: 400
+	},
+	{
+		name: "metal",
+		id: "3",
+		image: metalTile,
+		destructable: false,
+		hp: 0
+	},
+	{
+		name: "wood",
+		id: "4",
+		image: woodTile,
+		destructable: true,
+		hp: 10
+	}
+];
+
+export const BlockTypes: Array<BlockType> = blockTypeConfig.map(
+	c => new BlockType(c)
+);
+
+export const BlockTypeMap: Object = BlockTypes.reduce(
+	(output: Object, type: BlockType) => {
+		if (output[type.id]) {
+			throw new Error("duplicate id for block type");
+		}
+		output[type.id] = type;
+		return output;
+	},
+	{}
+);
