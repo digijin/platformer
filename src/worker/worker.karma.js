ae7cbbdd50764a5f5dc47eda3f99f1715bf3cb07
digi <james@digijin.com>
2017-12-13 21:29:47 +1100

    testing canvas transmission

--- src/worker/worker.karma.js
+++ src/worker/worker.karma.js
@@ -2,7 +2,7 @@ import { setTimeout } from "core-js/library/web/timers";
 
 const workerPath = "/base/src/worker/base.worker.js";
 
-fdescribe("webworker", () => {
+fdescribe("worker.karma.js webworker basics", () => {
 	let worker;
 	it("should be defined", () => {
 		expect(window.Worker).toBeDefined();
@@ -19,20 +19,36 @@ fdescribe("webworker", () => {
 		};
 		worker.postMessage({ action: "availability" });
 	});
-	it("should return a message", () => {
+	it("should return a message", done => {
 		worker = new Worker(workerPath);
 		worker.onmessage = e => {
 			expect(e.data).toBe("abc123");
 			done();
 		};
-		worker.postMessage({ repeat: "abc123" });
+		worker.postMessage({ action: "repeat", data: "abc123" });
 	});
-	// it("should return a fucking canvas", () => {
-	// 	worker = new Worker(workerPath);
-	// 	worker.onmessage = e => {
-	// 		expect(e.data).toBe("abc123");
-	// 		done();
-	// 	};
-	// 	worker.postMessage({ repeat: document.createElement("canvas") });
-	// });
+	it("should return a fucking canvas", done => {
+		let canvas = document.createElement("canvas");
+		let context = canvas.getContext("2d");
+
+		context.fillStyle = "black";
+		context.fillRect(10, 10, 100, 100);
+
+		let canvasData = context.getImageData(
+			0,
+			0,
+			canvas.width,
+			canvas.height
+		);
+
+		worker = new Worker(workerPath);
+		worker.onmessage = e => {
+			let returnCanvasData = e.data;
+			expect(returnCanvasData.data.toString()).toBe(
+				canvasData.data.toString()
+			);
+			done();
+		};
+		worker.postMessage({ action: "repeat", data: canvasData });
+	});
 });
