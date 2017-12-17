d066fd7fc26063c305f0a767006971154039f789
digijin <james@digijin.com>
2017-12-17 08:32:39 +0000

    player load panel

--- src/UI/MainMenu/Load.js
+++ src/UI/MainMenu/Load.js
@@ -1,14 +1,67 @@
 import React from "react";
 
 import Storage from "Storage";
+import { withStyles } from "material-ui/styles";
+import TextField from "material-ui/TextField";
 
-export default class Load extends React.Component {
+const styles = theme => ({
+	panel: {
+		borderRadius: "8px",
+		backgroundColor: "white",
+		boxShadow: "0px 0px 8px white",
+		border: "1px solid grey",
+		maxWidth: "400px",
+		margin: "0 auto"
+	},
+	saveList: {
+		backgroundColor: "black",
+		minHeight: "40px",
+		width: "100%",
+		marginBottom: "10px"
+	},
+	header: {
+		height: 24
+	}
+});
+class Load extends React.Component {
 	constructor() {
 		super();
 		this.storage = new Storage("src/saves");
+		this.state = { savename: "" };
 	}
+	changeSavename = e => {
+		this.setState({ savename: e.target.value });
+	};
 	render() {
-		console.log(this.storage.list());
-		return <div>load</div>;
+		const { classes } = this.props;
+		// console.log(this.storage.list());
+
+		return (
+			<div className={classes.panel}>
+				<div className={classes.header}>Choose Character</div>
+				<div className={classes.saveList}>
+					{this.storage.list().map(s => {
+						<a key={s}>{s}</a>;
+					})}
+				</div>
+				<TextField
+					label="Character Name"
+					placeholder="Enter your name"
+					className={classes.textField}
+					margin="normal"
+					value={this.savename}
+					onChange={this.changeSavename}
+				/>
+				<a
+					onClick={() => {
+						console.log(this.state.savename);
+					}}
+				>
+					add new
+				</a>
+			</div>
+		);
 	}
 }
+
+export default withStyles(styles)(Load);
