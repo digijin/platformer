6858b380973d41fcaa4b061253e6959d3784595a
digijin <james@digijin.com>
2017-12-18 04:39:38 +0000

    some navigation

--- src/UI/Briefing.js
+++ src/UI/Briefing.js
@@ -1,5 +1,8 @@
 //@flow
 import React from "react";
+import Button from "material-ui/Button";
+import MainMenu from "Scene/MainMenu";
+import Equip from "Scene/Equip";
 import { withStyles } from "material-ui/styles";
 const styles = theme => ({
 	panel: { backgroundColor: "white" }
@@ -7,7 +10,29 @@ const styles = theme => ({
 export class Briefing extends React.Component {
 	render() {
 		const { classes } = this.props;
-		return <div className={classes.panel}>Briefing panel here</div>;
+		return (
+			<div className={classes.panel}>
+				Briefing panel here
+				<Button
+					raised
+					className={classes.button}
+					onClick={() => {
+						this.props.engine.startScene(new MainMenu());
+					}}
+				>
+					go back to menu
+				</Button>
+				<Button
+					raised
+					className={classes.button}
+					onClick={() => {
+						this.props.engine.startScene(new Equip());
+					}}
+				>
+					go to equip screen
+				</Button>
+			</div>
+		);
 	}
 }
 export default withStyles(styles)(Briefing);
--- src/UI/Results.js
+++ src/UI/Results.js
@@ -1,5 +1,7 @@
 //@flow
 import React from "react";
+import Button from "material-ui/Button";
+import MainMenu from "Scene/MainMenu";
 import { withStyles } from "material-ui/styles";
 const styles = theme => ({
 	panel: { backgroundColor: "white" }
@@ -7,7 +9,20 @@ const styles = theme => ({
 export class Results extends React.Component {
 	render() {
 		const { classes } = this.props;
-		return <div className={classes.panel}>results panel here</div>;
+		return (
+			<div className={classes.panel}>
+				results panel here
+				<Button
+					raised
+					className={classes.button}
+					onClick={() => {
+						this.props.engine.startScene(new MainMenu());
+					}}
+				>
+					go back to menu
+				</Button>
+			</div>
+		);
 	}
 }
 export default withStyles(styles)(Results);
