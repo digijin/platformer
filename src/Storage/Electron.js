295c13fbe98583b4f94abdf89155f382539bc8c4
digijin <james@digijin.com>
2017-12-20 10:26:45 +0000

    added electron fs remove

--- src/Storage/Electron.js
+++ src/Storage/Electron.js
@@ -26,9 +26,20 @@ export default class ElectronAdapter extends StorageAdapter {
 		return this.path.resolve(this.savedir, name + ".txt");
 	}
 	save(name: string, data: string) {
+		console.log("write", this.saveFileName(name));
 		return this.fs.writeFileSync(this.saveFileName(name), data);
 	}
-	load(name: string): string {
+	load(name: string): string | null {
+		if (!this.fs.existsSync(this.saveFileName(name))) {
+			return null;
+		}
 		return this.fs.readFileSync(this.saveFileName(name)).toString();
+	}
+	remove(name: string) {
+		console.log("remove", this.saveFileName(name));
+		if (this.fs.existsSync(this.saveFileName(name))) {
+			console.log(this.saveFileName(name), "exists apparently");
+			return this.fs.unlinkSync(this.saveFileName(name));
+		}
 	}
 }
