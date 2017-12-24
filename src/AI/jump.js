038a9f0f795ccd940d2795a0d6eb61a687173bc0
digi <james@digijin.com>
2017-12-25 00:15:32 +1100

    rabbit generator regularly exiting

--- src/AI/jump.js
+++ src/AI/jump.js
@@ -2,14 +2,13 @@ import walk from "./walk";
 
 export default function* jump(
 	enemy: Enemy,
-	engine: Engine,
-	direction: number
+	engine: Engine
 ): Generator<*, *, *> {
 	const dontFall = true;
 
 	enemy.v = -4;
 
-	while (enemy.v) {
+	while (enemy.v !== 0) {
 		walk(...arguments);
 		enemy.gravity();
 		yield;
--- src/AI/walk.js
+++ src/AI/walk.js
@@ -1,10 +1,6 @@
-export default function walk(
-	enemy: Enemy,
-	engine: Engine,
-	direction: number
-): { status: string } {
+export default function walk(enemy: Enemy, engine: Engine): { status: string } {
 	const dontFall = true;
-	let hDelta = engine.deltaTime * enemy.walkSpeed * direction;
+	let hDelta = engine.deltaTime * enemy.walkSpeed * enemy.direction;
 	if (!enemy.canMoveHori(hDelta)) {
 		// direction = -direction;
 		return { status: "blocked" };
@@ -15,7 +11,7 @@ export default function walk(
 		let rect = enemy.getBoundingRect();
 
 		let check = { y: rect.b + 1, x: rect.r + 1 };
-		if (direction == -1) {
+		if (enemy.direction == -1) {
 			check.x = rect.l - 1;
 		}
 
