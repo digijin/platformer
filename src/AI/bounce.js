55bd123e08d7e62f01aaa562fa148cc1c8a10927
digi <james@digijin.com>
2017-12-25 00:46:26 +1100

    bounce returning often

--- src/AI/bounce.js
+++ src/AI/bounce.js
@@ -1,21 +1,29 @@
+//@flow
+import jump from "./jump";
+
+import type Enemy from "Enemy";
+import type Engine from "Engine";
 export default function* rabbit(
 	enemy: Enemy,
 	engine: Engine
 ): Generator<*, *, *> {
-	let direction = 1;
-	while (true) {
-		let hDelta = engine.deltaTime * enemy.walkSpeed * direction;
-		if (!enemy.canMoveHori(hDelta)) {
-			direction = -direction;
-		} else {
-			enemy.position.x += hDelta;
-		}
+	// let direction = 1;
+	// while (true) {
+	// 	let hDelta = engine.deltaTime * enemy.walkSpeed * direction;
+	// 	if (!enemy.canMoveHori(hDelta)) {
+	// 		direction = -direction;
+	// 	} else {
+	// 		enemy.position.x += hDelta;
+	// 	}
 
-		if (enemy.v == 0) {
-			//jump
-			enemy.v = -4;
-		}
-		enemy.gravity();
-		yield;
-	}
+	// 	if (enemy.v == 0) {
+	// 		//jump
+	// 		enemy.v = -4;
+	// 	}
+	// 	enemy.gravity();
+	// 	yield;
+	// }
+	enemy.direction = Math.random() > 0.5 ? 1 : -1;
+
+	yield* jump(...arguments);
 }
