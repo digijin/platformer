227058f6bf3f2b06d12a98f987230836a9d9b4c2
digi <james@digijin.com>
2017-12-25 00:51:26 +1100

    patrol exiting regularly

--- src/AI/patrol.js
+++ src/AI/patrol.js
@@ -5,14 +5,11 @@ export default function* patrol(
 	engine: Engine
 ): Generator<*, *, *> {
 	const dontFall = true;
-	let direction = 1;
-	while (true) {
-		let result = walk(enemy, engine, direction);
-		if (result.status !== "ok") {
-			direction = -direction;
-		}
 
-		enemy.gravity();
-		yield;
+	let result = walk(enemy, engine);
+	if (result.status !== "ok") {
+		enemy.direction = -enemy.direction;
 	}
+
+	enemy.gravity();
 }
