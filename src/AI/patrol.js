d80746b4e66f0405cef8271a792634faff988268
digi <james@digijin.com>
2017-12-24 18:07:05 +1100

    extracting out walk function

--- src/AI/patrol.js
+++ src/AI/patrol.js
@@ -1,3 +1,5 @@
+import walk from "./walk";
+
 export default function* patrol(
 	enemy: Enemy,
 	engine: Engine
@@ -5,23 +7,9 @@ export default function* patrol(
 	const dontFall = true;
 	let direction = 1;
 	while (true) {
-		let hDelta = engine.deltaTime * enemy.walkSpeed * direction;
-		if (!enemy.canMoveHori(hDelta)) {
+		let result = walk(enemy, engine, direction);
+		if (result.status !== "ok") {
 			direction = -direction;
-		} else {
-			enemy.position.x += hDelta;
-		}
-		if (dontFall) {
-			let rect = enemy.getBoundingRect();
-
-			let check = { y: rect.b + 1, x: rect.r + 1 };
-			if (direction == -1) {
-				check.x = rect.l - 1;
-			}
-
-			if (!engine.grid.isPositionBlocked(check)) {
-				direction = -direction;
-			}
 		}
 
 		enemy.gravity();
---
+++ src/AI/walk.js
@@ -0,0 +1,27 @@
+export default function walk(
+	enemy: Enemy,
+	engine: Engine,
+	direction: number
+): { status: string } {
+	const dontFall = true;
+	let hDelta = engine.deltaTime * enemy.walkSpeed * direction;
+	if (!enemy.canMoveHori(hDelta)) {
+		// direction = -direction;
+		return { status: "blocked" };
+	} else {
+		enemy.position.x += hDelta;
+	}
+	if (dontFall) {
+		let rect = enemy.getBoundingRect();
+
+		let check = { y: rect.b + 1, x: rect.r + 1 };
+		if (direction == -1) {
+			check.x = rect.l - 1;
+		}
+
+		if (!engine.grid.isPositionBlocked(check)) {
+			return { status: "gap" };
+		}
+	}
+	return { status: "ok" };
+}
