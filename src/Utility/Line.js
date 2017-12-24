f11da503af3f5b7837519296e0972ecf5ac967b1
digi <james@digijin.com>
2017-12-25 01:50:24 +1100

    line notes

---
+++ src/Utility/Line.js
@@ -0,0 +1,17 @@
+//@flow
+//UNTESTED - framework
+
+import Point from "./Point";
+import Rect from "./Rect";
+
+export default class Line {
+	a: Point;
+	b: Point;
+
+	constructor(params: { a: Point, b: Point }) {
+		this.a = params.a;
+		this.b = params.b;
+	}
+	percent(pc: number): Point {}
+	intersectsRect(rect: Rect): Boolean {}
+}
