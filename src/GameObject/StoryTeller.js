45a137acec42e70de18b6e6d84b913a0d57b5964
digi <james@digijin.com>
2017-12-30 22:59:20 +1100

    linked to results screen

--- src/GameObject/StoryTeller.js
+++ src/GameObject/StoryTeller.js
@@ -2,10 +2,11 @@ import GameObject from "GameObject";
 
 import Doors from "Transition/Doors";
 import MainMenu from "Scene/MainMenu";
+import Results from "Scene/Results";
 export default class StoryTeller extends GameObject {
 	update() {
 		if (this.engine.objectsTagged("enemy").length == 0) {
-			this.engine.startSceneTransition(new MainMenu(), new Doors());
+			this.engine.startSceneTransition(new Results(), new Doors());
 		}
 	}
 }
