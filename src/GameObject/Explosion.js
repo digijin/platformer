e1e53fca41847a4323d9da8d0c9b5134d8ee6d1c
digi <james@digijin.com>
2018-01-09 00:16:54 +1100

    explosions pixified

--- src/GameObject/Explosion.js
+++ src/GameObject/Explosion.js
@@ -7,6 +7,8 @@ import type Engine from "Engine";
 
 import GameObject from "GameObject";
 
+import * as PIXI from "pixi.js";
+
 export default class Explosion extends GameObject {
 	position: Point;
 	time: number; //life
@@ -22,7 +24,27 @@ export default class Explosion extends GameObject {
 
 		Object.assign(this, params);
 	}
+	init(engine) {
+		super.init(engine);
+		this.texture = new PIXI.Texture(new PIXI.BaseTexture(explosion));
+		this.sprite = new PIXI.Sprite(this.texture);
+		this.sprite.anchor = { x: 0.5, y: 0.5 };
+		this.engine.stage.addChild(this.sprite);
+		this.positionSprite();
+	}
+	positionSprite() {
+		this.sprite.rotation = this.rotation;
+		this.sprite.position.x = this.position.x - this.engine.view.offset.x;
+		this.sprite.position.y = this.position.y - this.engine.view.offset.y;
+		this.sprite.width = this.sprite.height =
+			this.size * Math.cos((1 - this.time) * Math.PI / 2);
+	}
+	destroy() {
+		this.engine.stage.removeChild(this.sprite);
+		super.destroy();
+	}
 	update(engine: Engine) {
+		this.positionSprite();
 		if (this.delay > 0) {
 			this.delay -= engine.deltaTime;
 		} else {
@@ -30,12 +52,12 @@ export default class Explosion extends GameObject {
 
 			let scale = Math.cos((1 - this.time) * Math.PI / 2);
 
-			engine.ctx.drawSprite(
-				explosion,
-				this.position,
-				{ w: this.size * scale, h: this.size * scale },
-				this.rotation
-			);
+			// engine.ctx.drawSprite(
+			// 	explosion,
+			// 	this.position,
+			// 	{ w: this.size * scale, h: this.size * scale },
+			// 	this.rotation
+			// );
 
 			if (this.time < 0) {
 				this.destroy();
