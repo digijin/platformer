5487881f947ba94a0aa0a99ff86a1a8d61108349
digi <james@digijin.com>
2018-01-26 22:41:03 +1100

    faster missiles

--- src/GameObject/Missile.js
+++ src/GameObject/Missile.js
@@ -18,128 +18,137 @@ import GameObject from "GameObject";
 import Projectile from "GameObject/Projectile";
 
 export default class Missile extends Projectile {
-	guided: boolean;
-	remoteControl: boolean;
-	trajectory: Line;
-	constructor(params : Object) {
-		super(params);
-		// this.speed = 1;
-		this.guided = true;
-		this.tag("missile");
-	}
-	init(engine : Engine) {
-		super.init(engine);
-	}
-	explode() {
-		super.explode();
+    guided: boolean;
+    remoteControl: boolean;
+    trajectory: Line;
+    maxSpeed: number = 16;
+    minSpeed: number = 1;
+    acceleration: number = 20;
+    constructor(params: Object) {
+        super(params);
+        this.guided = true;
+        this.remoteControl = false;
+        this.tag("missile");
+    }
+    init(engine: Engine) {
+        super.init(engine);
+    }
+    explode() {
+        super.explode();
+        this.engine.register(
+            new ExplosionRadial({
+                position: this.position,
+                rotation: 0,
+                delay: 0
+            })
+        );
+    }
+    move() {
+        let old = this.position.clone();
+        this.position.y += Math.sin(this.direction) * this.speed;
+        this.position.x += Math.cos(this.direction) * this.speed;
+        this.trajectory = new Line({ a: old, b: this.position });
+    }
+    update = () => {
+        this.move();
 
-		this.engine.register(new ExplosionRadial({position: this.position, rotation: 0, delay: 0}));
+        //CHECK GRID
+        this.checkGrid();
+        //CHECK ENEMIES
+        //USING EVERY SO I DONT EXPLODE MULTIPLE TIMES
+        this.checkActors();
 
-	}
-	move() {
-		let old = this.position.clone();
-		this.position.y += Math.sin(this.direction) * this.speed;
-		this.position.x += Math.cos(this.direction) * this.speed;
-		this.trajectory = new Line({a: old, b: this.position});
-	}
-	update = () => {
-		this.move();
+        //smoke trail
+        this.engine.register(new Smoke({ position: this.position.clone() }));
+        this.engine.register(
+            new Smoke({ position: this.trajectory.percent(0.5) })
+        );
 
-		this.remoteControl = false;
+        //aim at target
+        if (this.guided) {
+            if (this.remoteControl) {
+                this.target = this.engine.mouse.point;
+            }
+            let diff = this.target.subtract(this.position);
+            let dist = Math.pow(diff.x, 2) + Math.pow(diff.y, 2);
+            if (dist < config.missile.guidedDist) {
+                this.guided = false;
+            }
+            let newdir = Math.atan2(diff.y, diff.x);
 
-		//CHECK GRID
-		this.checkGrid();
-		//CHECK ENEMIES
-		//USING EVERY SO I DONT EXPLODE MULTIPLE TIMES
-		this.checkActors();
+            let dirDiff = this.direction - newdir;
+            if (dirDiff > Math.PI) newdir += 2 * Math.PI;
+            if (dirDiff < -Math.PI) newdir -= 2 * Math.PI;
 
-		//smoke trail
-		this.engine.register(new Smoke({position: this.position.clone()}));
-		this.engine.register(new Smoke({position: this.trajectory.percent(0.5)}));
+            //recalculate now we have removed cyclic variance
+            dirDiff = newdir - this.direction;
 
-		//aim at target
-		if (this.guided) {
-			if (this.remoteControl) {
-				this.target = this.engine.mouse.point;
-			}
-			let diff = this.target.subtract(this.position);
-			let dist = Math.pow(diff.x, 2) + Math.pow(diff.y, 2);
-			if (dist < config.missile.guidedDist) {
-				this.guided = false;
-			}
-			let newdir = Math.atan2(diff.y, diff.x);
+            //TODO REMOVE HARDCODING
+            if (Math.abs(dirDiff) < 0.5) {
+                this.speed += this.engine.deltaTime * this.acceleration;
+                this.direction += dirDiff / 3;
+            } else {
+                this.speed -= this.engine.deltaTime * this.acceleration / 3;
+                // this.speed = (this.speed + 1) /2;
+                if (dirDiff > 0) {
+                    this.direction += this.engine.deltaTime * Math.PI * 2;
+                } else {
+                    this.direction -= this.engine.deltaTime * Math.PI * 2;
+                }
+            }
+            if (this.speed < this.minSpeed) {
+                this.speed = this.minSpeed;
+            }
+            if (this.speed > this.maxSpeed) {
+                this.speed = this.maxSpeed;
+            }
 
-			let dirDiff = this.direction - newdir;
-			if (dirDiff > Math.PI) 
-				newdir += 2 * Math.PI;
-			if (dirDiff < -Math.PI) 
-				newdir -= 2 * Math.PI;
-			
-			//recalculate now we have removed cyclic variance
-			dirDiff = newdir - this.direction;
+            //dont spin it up too much
+            if (this.direction > Math.PI) {
+                this.direction -= Math.PI * 2;
+            }
+            if (this.direction < -Math.PI) {
+                this.direction += Math.PI * 2;
+            }
+        }
 
-			//TODO REMOVE HARDCODING
-			if (Math.abs(dirDiff) < 0.5) {
-				this.speed += this.engine.deltaTime * 16;
-				this.direction += dirDiff / 3;
-			} else {
-				this.speed -= this.engine.deltaTime * 5;
-				// this.speed = (this.speed + 1) /2;
-				if (dirDiff > 0) {
-					this.direction += this.engine.deltaTime * Math.PI * 2;
-				} else {
-					this.direction -= this.engine.deltaTime * Math.PI * 2;
-				}
-			}
-			if (this.speed < 1) 
-				this.speed = 1;
-			if (this.speed > 12) 
-				this.speed = 12;
-			
-			//dont spin it up too much
-			if (this.direction > Math.PI) 
-				this.direction -= Math.PI * 2;
-			if (this.direction < -Math.PI) 
-				this.direction += Math.PI * 2;
-			}
-		
-		this.engine.ctx.drawSprite(missile, this.position, {
-			w: 20,
-			h: 10
-		}, this.direction, {
-			x: 0.2,
-			y: 0.5
-		});
-	};
+        // this.engine.ctx.drawSprite(missile, this.position, {
+        // 	w: 20,
+        // 	h: 10
+        // }, this.direction, {
+        // 	x: 0.2,
+        // 	y: 0.5
+        // });
+    };
 
-	checkActors() {
-		this.engine.objectsTagged("actor").every((o : GameObject) => {
-			if (o !== this.owner) {
-				let a: Actor = ((o : any) : Actor); //RECAST
-				if (a.getBoundingRect().contains(this.position)) {
-					this.explode();
-					// a.explode();
-					a.damage(30 + Math.random() * 75);
-					return false;
-				}
-			}
-			return true;
-		});
-	}
+    checkActors() {
+        this.engine.objectsTagged("actor").every((o: GameObject) => {
+            if (o !== this.owner) {
+                let a: Actor = ((o: any): Actor); //RECAST
+                if (a.getBoundingRect().contains(this.position)) {
+                    this.explode();
+                    // a.explode();
+                    a.damage(30 + Math.random() * 75);
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
 
-	checkGrid() {
-		let block = this.engine.grid.getBlockAtPoint(this.position);
-		if (block && !block.isEmpty()) {
-			this.explode();
-			let r = 15;
-			let rect = new Rect({
-				t: this.position.y - r,
-				r: this.position.x + r,
-				b: this.position.y + r,
-				l: this.position.x - r
-			});
-			let blocks = this.engine.grid.getBlocksOverlappingRect(rect);
-			blocks.forEach(b => b.damage(10 + Math.random() * 100));
-		}
-	}
+    checkGrid() {
+        let block = this.engine.grid.getBlockAtPoint(this.position);
+        if (block && !block.isEmpty()) {
+            this.explode();
+            let r = 15;
+            let rect = new Rect({
+                t: this.position.y - r,
+                r: this.position.x + r,
+                b: this.position.y + r,
+                l: this.position.x - r
+            });
+            let blocks = this.engine.grid.getBlocksOverlappingRect(rect);
+            blocks.forEach(b => b.damage(10 + Math.random() * 100));
+        }
+    }
 }
--- src/GameObject/Smoke.js
+++ src/GameObject/Smoke.js
@@ -8,77 +8,76 @@ import smoke from "assets/smoke.png";
 import GameObject from "GameObject";
 import RGBA from "Utility/RGBA";
 
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js";
 
 export default class Smoke extends GameObject {
-	position: Point;
-	time: number; //life
-	duration: number; //amount of sec this thing plays for
-	rotation: number; //radians
-	texture: PIXI.Texture;
-	sprite: PIXI.Sprite;
-	h: number;
-	v: number;
-	constructor(params : Object) {
-		super();
-		this.duration = 0.5
-		Object.assign(this, params);
+    position: Point;
+    time: number; //life
+    duration: number; //amount of sec this thing plays for
+    rotation: number; //radians
+    texture: PIXI.Texture;
+    sprite: PIXI.Sprite;
+    h: number;
+    v: number;
+    constructor(params: Object) {
+        super();
+        this.duration = 0.3;
+        Object.assign(this, params);
 
-		this.time = this.duration;
-		this.rotation = Math.random() * Math.PI * 2;
-	}
-	init(engine : Engine) {
-		super.init(engine);
-		this.texture = new PIXI.Texture(new PIXI.BaseTexture(smoke));
-		// this.texture = PIXI.Texture.WHITE;
-		this.sprite = new PIXI.Sprite(this.texture);
-		this.sprite.anchor = {
-			x: 0.5,
-			y: 0.5
-		};
-		this.engine.stage.addChild(this.sprite);
+        this.time = this.duration;
+        this.rotation = Math.random() * Math.PI * 2;
+    }
+    init(engine: Engine) {
+        super.init(engine);
+        this.texture = new PIXI.Texture(new PIXI.BaseTexture(smoke));
+        // this.texture = PIXI.Texture.WHITE;
+        this.sprite = new PIXI.Sprite(this.texture);
+        this.sprite.anchor = {
+            x: 0.5,
+            y: 0.5
+        };
+        this.engine.stage.addChild(this.sprite);
 
-		this.h = Math.random() - 0.5;
-		this.v = Math.random() - 0.5;
-		this.positionSprite();
-	}
-	positionSprite() {
-		let timePc = this.time / this.duration;
-		this.position.x += this.engine.deltaTime * this.h;
-		this.position.y += this.engine.deltaTime * this.v;
-		this.sprite.rotation = this.rotation;
-		this.sprite.position.x = this.position.x;
-		this.sprite.position.y = this.position.y;
-		this.sprite.width = this.sprite.height = 10 + timePc * 10;
-		this.sprite.alpha = timePc;
-		this.sprite.tint = new RGBA({
-			r: 1,
-			g: 1,
-			b: 1 - timePc,
-			a: 1
-		}).toNumber();
-	}
-	exit() {
+        this.h = Math.random() - 0.5;
+        this.v = Math.random() - 0.5;
+        this.positionSprite();
+    }
+    positionSprite() {
+        let timePc = this.time / this.duration;
+        this.position.x += this.engine.deltaTime * this.h;
+        this.position.y += this.engine.deltaTime * this.v;
+        this.sprite.rotation = this.rotation;
+        this.sprite.position.x = this.position.x;
+        this.sprite.position.y = this.position.y;
+        this.sprite.width = this.sprite.height = 10 + timePc * 10;
+        this.sprite.alpha = timePc;
+        this.sprite.tint = new RGBA({
+            r: 1,
+            g: 1,
+            b: 1 - timePc,
+            a: 1
+        }).toNumber();
+    }
+    exit() {
+        this.engine.stage.removeChild(this.sprite);
+    }
+    destroy() {
+        this.exit();
+        super.destroy();
+    }
 
-		this.engine.stage.removeChild(this.sprite);
-	}
-	destroy() {
-		this.exit();
-		super.destroy();
-	}
+    update = (engine: Engine) => {
+        let timePc = this.time / this.duration;
+        this.positionSprite();
+        this.time -= engine.deltaTime;
 
-	update = (engine : Engine) => {
-		let timePc = this.time / this.duration;
-		this.positionSprite();
-		this.time -= engine.deltaTime;
+        let w = 20;
+        let h = 20;
+        h *= timePc;
+        w *= timePc;
 
-		let w = 20;
-		let h = 20;
-		h *= timePc;
-		w *= timePc;
-
-		if (this.time < 0) {
-			this.destroy();
-		}
-	};
+        if (this.time < 0) {
+            this.destroy();
+        }
+    };
 }
