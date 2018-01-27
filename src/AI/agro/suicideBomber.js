69c437ba87dc3c8d7f07afd8032e94e0a9ec3683
digi <james@digijin.com>
2018-01-27 17:41:33 +1100

    new bomber agro type

---
+++ src/AI/agro/suicideBomber.js
@@ -0,0 +1,47 @@
+import type Player from "Actor/Player";
+import Missile from "GameObject/Missile";
+const CLOSEST_DISTANCE = 50;
+
+export default function* suicideBomber(
+    enemy: Enemy,
+    engine: Engine,
+    player: Player
+): Generator<*, *, *> {
+    const dontFall = true;
+    let direction = 1;
+    let firingCooldown = 0;
+
+    let tryJump = () => {
+        if (enemy.v == 0) {
+            enemy.v = -4;
+        }
+    };
+    while (true) {
+        enemy.direction = player.position.x < enemy.position.x ? -1 : 1;
+        let distance = player.position.distanceTo(enemy.position);
+        let hDelta = engine.deltaTime * enemy.walkSpeed * enemy.direction;
+        if (distance < CLOSEST_DISTANCE) {
+            tryJump();
+        }
+        if (!enemy.canMoveHori(hDelta)) {
+            tryJump();
+        } else {
+            enemy.position.x += hDelta;
+        }
+        if (dontFall) {
+            let rect = enemy.getBoundingRect();
+
+            let check = { y: rect.b + 4, x: rect.r + 1 };
+            if (enemy.direction == -1) {
+                check.x = rect.l - 1;
+            }
+
+            if (!engine.grid.isPositionBlocked(check)) {
+                tryJump();
+            }
+        }
+
+        enemy.gravity();
+        yield;
+    }
+}
--- src/Actor/Enemy/Type.js
+++ src/Actor/Enemy/Type.js
@@ -4,122 +4,127 @@ import mech from "assets/mech.png";
 import heli from "assets/heli.png";
 
 export type EnemyTypeParams = {
-	// hp: name,
-	name: string,
-	walkSpeed: number,
-	jumpSpeed: number,
-	hp: number,
-	size: {
-		w: number,
-		h: number
-	},
-	registration: {
-		x: number,
-		y: number
-	},
-	idle: string,
-	agro: string
+    // hp: name,
+    name: string,
+    walkSpeed: number,
+    jumpSpeed: number,
+    hp: number,
+    size: {
+        w: number,
+        h: number
+    },
+    registration: {
+        x: number,
+        y: number
+    },
+    idle: string,
+    agro: string
 };
 
 export default class EnemyType {
-	name: string;
-	walkSpeed: number;
-	jumpSpeed: number;
-	hp: number;
-	size: {
-		w: number,
-		h: number
-	};
-	registration: {
-		x: number,
-		y: number
-	};
-	idle: string;
-	agro: string;
-	image: any;
-	id: string;
-	constructor(params : EnemyTypeParams) {
-		Object.assign(this, params);
-	}
+    name: string;
+    walkSpeed: number;
+    jumpSpeed: number;
+    hp: number;
+    size: {
+        w: number,
+        h: number
+    };
+    registration: {
+        x: number,
+        y: number
+    };
+    idle: string;
+    agro: string;
+    image: any;
+    id: string;
+    constructor(params: EnemyTypeParams) {
+        Object.assign(this, params);
+    }
 }
 
 let enemyTypeConfig: Array<EnemyTypeParams> = [
-	{
-		id: "1",
-		name: "hopper",
-		walkSpeed: 50,
-		jumpSpeed: 100,
-		hp: 50,
-		size: {
-			w: 50,
-			h: 50
-		},
-		registration: {
-			x: 0.5,
-			y: 1
-		},
-		idle: "rabbit",
-		agro: "agro",
-		image: mech
-	}, {
-		id: "2",
-		name: "lil bouncy",
-		walkSpeed: 50,
-		jumpSpeed: 100,
-		hp: 1,
-		size: {
-			w: 20,
-			h: 20
-		},
-		registration: {
-			x: 0.5,
-			y: 1
-		},
-		idle: "bounce",
-		agro: "agro",
-		image: mech
-	}, {
-		id: "3",
-		name: "guard",
-		walkSpeed: 50,
-		jumpSpeed: 100,
-		hp: 100,
-		size: {
-			w: 60,
-			h: 60
-		},
-		registration: {
-			x: 0.5,
-			y: 1
-		},
-		idle: "patrol",
-		agro: "agro",
-		image: mech
-	}, {
-		id: "4",
-		name: "heli",
-		walkSpeed: 50,
-		jumpSpeed: 100,
-		hp: 100,
-		size: {
-			w: 124,
-			h: 36
-		},
-		registration: {
-			x: 0.5,
-			y: 1
-		},
-		idle: "hover",
-		agro: "heligun",
-		image: heli
-	}
+    {
+        id: "1",
+        name: "hopper",
+        walkSpeed: 50,
+        jumpSpeed: 100,
+        hp: 50,
+        size: {
+            w: 50,
+            h: 50
+        },
+        registration: {
+            x: 0.5,
+            y: 1
+        },
+        idle: "rabbit",
+        agro: "agro",
+        image: mech
+    },
+    {
+        id: "2",
+        name: "lil bouncy",
+        walkSpeed: 250,
+        jumpSpeed: 100,
+        hp: 1,
+        size: {
+            w: 20,
+            h: 20
+        },
+        registration: {
+            x: 0.5,
+            y: 1
+        },
+        idle: "bounce",
+        agro: "suicideBomber",
+        image: mech
+    },
+    {
+        id: "3",
+        name: "guard",
+        walkSpeed: 50,
+        jumpSpeed: 100,
+        hp: 100,
+        size: {
+            w: 60,
+            h: 60
+        },
+        registration: {
+            x: 0.5,
+            y: 1
+        },
+        idle: "patrol",
+        agro: "agro",
+        image: mech
+    },
+    {
+        id: "4",
+        name: "heli",
+        walkSpeed: 50,
+        jumpSpeed: 100,
+        hp: 100,
+        size: {
+            w: 124,
+            h: 36
+        },
+        registration: {
+            x: 0.5,
+            y: 1
+        },
+        idle: "hover",
+        agro: "heligun",
+        image: heli
+    }
 ];
 
-export const EnemyTypes: Array<EnemyType> = enemyTypeConfig.map(c => new EnemyType(c));
+export const EnemyTypes: Array<EnemyType> = enemyTypeConfig.map(
+    c => new EnemyType(c)
+);
 
 export const EnemyTypesMap: {
-	[id: string]: EnemyType
+    [id: string]: EnemyType
 } = EnemyTypes.reduce((out, next) => {
-	out[next.id] = next;
-	return out;
+    out[next.id] = next;
+    return out;
 }, {});
