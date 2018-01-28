468718b3469e8179e5a62edb6f717122c9d85389
digi <james@digijin.com>
2018-01-28 23:34:08 +1100

    bombers randomise butting heads against wall

--- src/AI/agro/suicideBomber.js
+++ src/AI/agro/suicideBomber.js
@@ -1,4 +1,5 @@
 import type Player from "Actor/Player";
+import Rect from "Utility/Rect";
 import Missile from "GameObject/Missile";
 const CLOSEST_DISTANCE = 50;
 const DETONATE_DISTANCE = 20;
@@ -14,21 +15,46 @@ export default function* suicideBomber(
 
     let tryJump = () => {
         if (enemy.v == 0) {
-            enemy.v = -enemy.type.jumpPower;
+            enemy.v = -(0.5 + Math.random() / 2) * enemy.type.jumpPower;
         }
     };
     while (true) {
         enemy.direction = player.position.x < enemy.position.x ? -1 : 1;
         let distance = player.position.distanceTo(enemy.position);
-        let hDelta = engine.deltaTime * enemy.walkSpeed * enemy.direction;
+        if (enemy.v == 0) {
+            //on ground
+            enemy.h = enemy.walkSpeed * enemy.direction;
+        }
+        let hDelta = engine.deltaTime * enemy.h;
         if (distance < DETONATE_DISTANCE) {
             enemy.explode();
         }
         if (distance < CLOSEST_DISTANCE) {
             tryJump();
         }
-        if (!enemy.canMoveHori(hDelta)) {
+
+        //upcoming obstacle
+        let rect = new Rect({
+            t: enemy.position.y - 10,
+            r: enemy.position.x + enemy.direction * 20 + 10,
+            l: enemy.position.x + enemy.direction * -20 - 10,
+            b: enemy.position.y - 1
+        });
+        if (
+            engine.grid
+                .getBlocksOverlappingRect(rect)
+                .every(block => !block.isEmpty())
+        ) {
+            //try jump over upcoming obstacle
             tryJump();
+        }
+
+        if (!enemy.canMoveHori(hDelta)) {
+            if (enemy.v == 0) {
+                tryJump();
+            } else {
+                enemy.h *= -0.5;
+            }
         } else {
             enemy.position.x += hDelta;
         }
