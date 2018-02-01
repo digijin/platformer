3157018df258d83c2245cb1e6086ca84c1f3a37a
digi <james@digijin.com>
2018-02-01 23:44:04 +1100

    images from pixi to react

--- src/Scene/Editor.js
+++ src/Scene/Editor.js
@@ -3,22 +3,26 @@ import Enemy from "Actor/Enemy";
 import Point from "Utility/Point";
 import Base from "./Base";
 import Grid from "Grid";
+import Background from "GameObject/BackgroundBuildings";
 
 import EditorWatcher from "Editor/Watcher";
 
 export default class Editor extends Base {
-	start(engine) {
-		super.start(engine);
-		let grid = new Grid({ w: 200, h: 50 });
-		// grid.makeTest();
-		grid.generate(1);
-		engine.register(grid);
+    start(engine) {
+        super.start(engine);
+        let grid = new Grid({ w: 200, h: 50 });
+        // grid.makeTest();
+        grid.generate(1);
+        engine.register(grid);
 
-		document.body.style.backgroundColor = "#87efff";
+        document.body.style.backgroundColor = "#87efff";
+        let bg = new Background();
+        bg.spawnExplosion = () => {};
+        engine.register(bg);
 
-		let watcher = new EditorWatcher();
-		engine.register(watcher);
+        let watcher = new EditorWatcher();
+        engine.register(watcher);
 
-		engine.ui.dispatch({ type: "START_SCENE", scene: "editor" });
-	}
+        engine.ui.dispatch({ type: "START_SCENE", scene: "editor" });
+    }
 }
