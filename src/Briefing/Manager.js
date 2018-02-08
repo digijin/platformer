ed079a77c8a465fee542b91ffadb7e677ed54535
digi <james@digijin.com>
2018-02-08 21:39:51 +1100

    centering ui and some reacty wiring

--- src/Briefing/Manager.js
+++ src/Briefing/Manager.js
@@ -9,16 +9,19 @@ import { Missions } from "Mission";
 
 import * as PIXI from "pixi.js";
 
+import Point from "Utility/Point";
+
 export default class BriefingManager extends GameObject {
     container: PIXI.Container;
-    missionPanel: PIXI.Container;
     missionsPanel: MissionsPanel;
     panelPadding: number = 10;
     init(engine: Engine) {
         super.init(engine);
         this.container = new PIXI.Container();
 
-        this.missionsPanel = new MissionsPanel();
+        this.missionsPanel = new MissionsPanel({
+            onMissionChange: this.onMissionChange
+        });
         this.engine.register(this.missionsPanel);
         this.container.addChild(this.missionsPanel.container);
 
@@ -30,6 +33,15 @@ export default class BriefingManager extends GameObject {
     exit() {
         this.engine.stage.removeChild(this.container);
     }
+    onMissionChange(mission) {
+        console.log("mission changed to", mission);
+    }
 
-    update() {}
+    update() {
+        let target = new Point({
+            x: (this.engine.renderer.width - this.container.width) / 2,
+            y: (this.engine.renderer.height - this.container.height) / 2
+        });
+        this.container.position = target;
+    }
 }
--- src/Briefing/MissionsPanel.js
+++ src/Briefing/MissionsPanel.js
@@ -7,10 +7,18 @@ import type Engine from "Engine";
 import Panel from "./Panel";
 import { Missions } from "Mission";
 
+import type Mission from "Mission";
+
+type Props = {
+    onMissionChange: mission => {}
+};
+
 export default class BriefingMissionsPanel extends Panel {
     heading: PIXI.Text;
     missionButtons: Array<PIXI.DisplayObject>;
-    constructor(props) {
+    props: Props;
+    selectedMission: Mission | null;
+    constructor(props: Props) {
         super();
         this.props = props;
     }
@@ -28,7 +36,7 @@ export default class BriefingMissionsPanel extends Panel {
     }
 
     addMissionButtons() {
-        Missions.forEach((mission, index) => {
+        this.missionButtons = Missions.map((mission, index) => {
             let text = new PIXI.Text(mission.title, {
                 fontFamily: "Arial",
                 fontSize: 18,
@@ -39,10 +47,12 @@ export default class BriefingMissionsPanel extends Panel {
             text.buttonMode = true;
             text.interactive = true;
             text.on("mousedown", event => {
-                console.log("text down", mission);
+                this.selectedMission = mission;
+                this.props.onMissionChange(mission);
             });
             text.position.y = 30 + index * 20;
             this.content.addChild(text);
+            return text;
         });
     }
 }
