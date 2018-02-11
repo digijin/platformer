aaeee419ba50bb4b3c29312d30561c8510d96299
digi <james@digijin.com>
2018-02-11 23:43:42 +1100

    tv turn off effect

--- src/Transition/BriefingEnd.js
+++ src/Transition/BriefingEnd.js
@@ -2,6 +2,7 @@
 import type Engine from "Engine";
 import Base from "./Base";
 
+import { AdvancedBloomFilter } from "@pixi/filter-advanced-bloom";
 import * as PIXI from "pixi.js";
 
 const SECS = 2;
@@ -24,22 +25,42 @@ export default class BriefingEnd extends Base {
         this.flash.beginFill(0xffffff).drawCircle(0, 0, 10);
         this.flash.x = window.innerWidth / 2;
         this.flash.y = window.innerHeight / 2;
+        this.flash.filters = [new AdvancedBloomFilter()];
         this.engine.stage.addChild(this.flash);
         this.manager = this.engine.objectsTagged("briefingmanager")[0];
     }
     swapped: boolean = false;
+    flashMaxWidth: number = 100;
     update() {
-        this.manager.container.transform.scale.x *= 0.9;
-        this.manager.container.transform.scale.y *= 0.9;
-        this.frame++;
-        this.time += this.engine.deltaTime;
-        if (this.time > SECS && !this.swapped) {
+        if (
+            this.manager.container.transform.scale.x > 0.1 &&
+            this.flash.width < this.flashMaxWidth - 1
+        ) {
+            this.manager.container.transform.scale.x *= 0.9;
+            this.manager.container.transform.scale.y *= 0.9;
+
+            let n = 8;
+
+            this.flash.width =
+                (this.flashMaxWidth + this.flash.width * n) / (n + 1);
+            this.flash.height =
+                (this.flashMaxWidth + this.flash.height * n) / (n + 1);
+        } else if (this.flash.height > 0) {
+            this.manager.container.visible = false;
+            this.flash.width += this.engine.deltaTime * 1500;
+            this.flash.height -= this.engine.deltaTime * 300;
+        } else {
             this.endLastScene();
             this.startNextScene();
-            this.swapped = true;
-        } else if (this.time > SECS * 2) {
             this.end();
         }
+
+        // this.frame++;
+        // this.time += this.engine.deltaTime;
+        // if (this.time > SECS && !this.swapped) {
+        //     this.swapped = true;
+        // } else if (this.time > SECS * 2) {
+        // }
     }
     end() {
         this.engine.stage.removeChild(this.flash);
