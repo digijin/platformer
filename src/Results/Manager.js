b6213f3a53b2fb6ed79c69ffe3b6c910680fbfc1
digi <james@digijin.com>
2018-03-12 00:13:50 +1100

    added up loop for results

--- src/Results/Manager.js
+++ src/Results/Manager.js
@@ -2,14 +2,23 @@
 import type Engine from "Engine";
 import GameObject from "GameObject";
 import * as PIXI from "pixi.js";
+import Point from "Utility/Point";
+import FireUpLoopTransition from "../GameObject/FireUpLoopTransition";
 export default class ResultsManager extends GameObject {
 	init(engine: Engine) {
 		super.init(engine);
 		this.container = new PIXI.Container();
 		this.engine.stage.addChild(this.container);
 
-		this.background = new PIXI.Sprite(PIXI.Texture.WHITE);
-		this.container.addChild(this.background);
+		// this.background = new PIXI.Sprite(PIXI.Texture.WHITE);
+		this.background = new FireUpLoopTransition({
+			parent: this.container,
+			position: new Point(),
+			rotation: 0,
+			delay: 0
+		});
+		this.engine.register(this.background);
+		// this.container.addChild(this.background);
 	}
 
 	exit() {
