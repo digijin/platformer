644b8919b84b4b985be9729b4fce7b4f9295b3aa
digi <james@digijin.com>
2018-06-03 21:25:35 +1000

    basic menu elements

--- src/Briefing/Rectangle.js
+++ src/Briefing/Rectangle.js
@@ -4,24 +4,41 @@ import Rect from "Utility/Rect";
 
 type Params = {
 	rect: Rect,
-	width?: number,
-	color: number,
+	border?: number,
+	borderColor: number,
 	alpha?: number,
-	border: boolean
+	fillColor?: number
 };
 
 export default class Rectangle extends PIXI.Graphics {
+	rect: Rect;
+
 	constructor(params: Params) {
 		super();
+		Object.assign(this, params);
+		this.render();
+	}
+
+	apply(params: Params) {
+		Object.assign(this, params);
+		this.render();
+	}
+
+	render() {
 		this.cacheAsBitmap = false;
 		// this.position.set(10, 10);
+
 		this.clear();
-		this.lineStyle(params.width, params.color)
-			.moveTo(params.rect.l, params.rect.t)
-			.lineTo(params.rect.l, params.rect.b)
-			.lineTo(params.rect.r, params.rect.b)
-			.lineTo(params.rect.r, params.rect.t)
-			.lineTo(params.rect.l, params.rect.t);
+
+		if (this.fillColor) {
+			this.beginFill(this.fillColor, this.alpha);
+		}
+		this.lineStyle(this.border, this.borderColor)
+			.moveTo(this.rect.l, this.rect.t)
+			.lineTo(this.rect.l, this.rect.b)
+			.lineTo(this.rect.r, this.rect.b)
+			.lineTo(this.rect.r, this.rect.t)
+			.lineTo(this.rect.l, this.rect.t);
 		this.cacheAsBitmap = true;
 	}
 }
