0d2f3d60c5808686f14d67d503915226c39e5e79
digi <james@digijin.com>
2018-06-08 22:31:11 +1000

    trying to get jest working

--- src/Actor/Enemy.js
+++ src/Actor/Enemy.js
@@ -6,7 +6,6 @@ import Rect from "Utility/Rect";
 import mech from "assets/mech.png";
 
 import Actor from "Actor";
-
 import config from "config";
 import type Player from "Actor/Player";
 
@@ -27,171 +26,171 @@ const DEAGRO_DISTANCE = 1000;
 class EnemySprite extends PIXI.Sprite {}
 
 export default class Enemy extends Actor {
-    sprite: PIXI.Sprite;
-    h: number;
-    parent: PIXI.Container;
-    v: number;
-    graph: PIXI.Graphics;
-    action: ?Generator<*, *, *>;
-    walkSpeed: number;
-    type: EnemyType;
-    agro: Player | null;
-    direction: number;
+	sprite: PIXI.Sprite;
+	h: number;
+	parent: PIXI.Container;
+	v: number;
+	graph: PIXI.Graphics;
+	action: ?Generator<*, *, *>;
+	walkSpeed: number;
+	type: EnemyType;
+	agro: Player | null;
+	direction: number;
 
-    constructor(params: { position: Point, type: EnemyType }) {
-    	super(params);
-    	this.hp = params.type.hp;
-    	this.maxhp = params.type.hp;
-    	this.tag("enemy");
-    	this.walkSpeed = params.type.walkSpeed; //config.enemy.walkSpeed;
-    	this.size = params.type.size;
-    	this.registration = params.type.registration;
-    	this.direction = 1;
-    	this.parent = params.parent;
-    	if (!this.size) {
-    		throw new Error("no size in enemy init");
-    	}
-    	if (!this.registration) {
-    		throw new Error("no rewgistration in enemy init");
-    	}
-    	if (!this.parent) {
-    		throw new Error("no parent in enemy init");
-    	}
-    	// this.size = config.enemy.size;
-    	// this.registration = config.enemy.registration;
+	constructor(params: { position: Point, type: EnemyType }) {
+		super(params);
+		this.hp = params.type.hp;
+		this.maxhp = params.type.hp;
+		this.tag("enemy");
+		this.walkSpeed = params.type.walkSpeed; //config.enemy.walkSpeed;
+		this.size = params.type.size;
+		this.registration = params.type.registration;
+		this.direction = 1;
+		this.parent = params.parent;
+		if (!this.size) {
+			throw new Error("no size in enemy init");
+		}
+		if (!this.registration) {
+			throw new Error("no rewgistration in enemy init");
+		}
+		if (!this.parent) {
+			throw new Error("no parent in enemy init");
+		}
+		// this.size = config.enemy.size;
+		// this.registration = config.enemy.registration;
 
-    	Object.assign(this, params);
-    }
+		Object.assign(this, params);
+	}
 
-    init(engine: Engine) {
-    	super.init(engine);
-    	let texture = new PIXI.Texture(new PIXI.BaseTexture(this.type.image));
-    	this.sprite = new EnemySprite(texture);
-    	this.sprite.anchor = this.registration;
-    	this.sprite.width = this.size.w;
-    	this.sprite.height = this.size.h;
-    	this.parent.addChild(this.sprite);
+	init(engine: Engine) {
+		super.init(engine);
+		let texture = new PIXI.Texture(new PIXI.BaseTexture(this.type.image));
+		this.sprite = new EnemySprite(texture);
+		this.sprite.anchor = this.registration;
+		this.sprite.width = this.size.w;
+		this.sprite.height = this.size.h;
+		this.parent.addChild(this.sprite);
 
-    	this.graph = new PIXI.Graphics();
-    	this.parent.addChild(this.graph);
-    }
+		this.graph = new PIXI.Graphics();
+		this.parent.addChild(this.graph);
+	}
 
-    exit() {
-    	this.parent.removeChild(this.sprite);
-    	this.parent.removeChild(this.graph);
-    }
+	exit() {
+		this.parent.removeChild(this.sprite);
+		this.parent.removeChild(this.graph);
+	}
 
-    update() {
-    	this.sprite.position = this.position;
-    	let player = this.engine.objectsTagged("player").pop();
-    	if (!player) {
-    		// this.gravity();
-    		this.render();
-    		return;
-    	} //do not movefor edit mode
-    	//check if out of bounds
-    	this.unstuck();
-    	//check agro
-    	// this.checkAgro(player);
-    	if (!this.action) {
-    		// switch(this.type.)
-    		this.newAction();
-    	} else {
-    		if (this.action.next().done) {
-    			this.action = null;
-    		}
-    	}
-    	this.render();
-    }
+	update() {
+		this.sprite.position = this.position;
+		let player = this.engine.objectsTagged("player").pop();
+		if (!player) {
+			// this.gravity();
+			this.render();
+			return;
+		} //do not movefor edit mode
+		//check if out of bounds
+		this.unstuck();
+		//check agro
+		// this.checkAgro(player);
+		if (!this.action) {
+			// switch(this.type.)
+			this.newAction();
+		} else {
+			if (this.action.next().done) {
+				this.action = null;
+			}
+		}
+		this.render();
+	}
 
-    destroy() {
-    	this.exit();
-    	super.destroy();
-    }
+	destroy() {
+		this.exit();
+		super.destroy();
+	}
 
-    newAction() {
-    	let player = this.engine.objectsTagged("player").pop();
-    	if (this.agro) {
-    		//falloff distance
-    		if (
-    			this.position.distanceTo(this.agro.position) > DEAGRO_DISTANCE
-    		) {
-    			this.startIdle();
-    		} else {
-    			this.startAgro(player);
-    		}
-    	} else {
-    		//pickup distance
-    		if (this.position.distanceTo(player.position) < AGRO_DISTANCE) {
-    			this.startAgro(player);
-    		} else {
-    			this.startIdle();
-    		}
-    	}
-    }
+	newAction() {
+		let player = this.engine.objectsTagged("player").pop();
+		if (this.agro) {
+			//falloff distance
+			if (
+				this.position.distanceTo(this.agro.position) > DEAGRO_DISTANCE
+			) {
+				this.startIdle();
+			} else {
+				this.startAgro(player);
+			}
+		} else {
+			//pickup distance
+			if (this.position.distanceTo(player.position) < AGRO_DISTANCE) {
+				this.startAgro(player);
+			} else {
+				this.startIdle();
+			}
+		}
+	}
 
-    unstuck() {
-    	if (this.position.y > this.engine.grid.width * config.grid.width * 2) {
-    		this.explode();
-    	}
-    	//if stuck
-    	if (
-    		this.engine.grid
-    			.getBlocksOverlappingRect(this.getBoundingRect())
-    			.filter(block => !block.isEmpty()).length > 0
-    	) {
-    		this.explode();
-    	}
-    }
+	unstuck() {
+		if (this.position.y > this.engine.grid.width * config.grid.width * 2) {
+			this.explode();
+		}
+		//if stuck
+		if (
+			this.engine.grid
+				.getBlocksOverlappingRect(this.getBoundingRect())
+				.filter(block => !block.isEmpty()).length > 0
+		) {
+			this.explode();
+		}
+	}
 
-    render() {
-    	this.graph.clear();
-    	this.graph.position.set(this.position.x, this.position.y);
-    	this.graph
-    		.lineStyle(5, 0xffffff)
-    		.moveTo(0, 0)
-    		.lineTo(20, 0);
-    	this.graph
-    		.lineStyle(3, 0x00ff00)
-    		.moveTo(0, 0)
-    		.lineTo(20 * this.hp / this.type.hp, 0);
-    }
+	render() {
+		this.graph.clear();
+		this.graph.position.set(this.position.x, this.position.y);
+		this.graph
+			.lineStyle(5, 0xffffff)
+			.moveTo(0, 0)
+			.lineTo(20, 0);
+		this.graph
+			.lineStyle(3, 0x00ff00)
+			.moveTo(0, 0)
+			.lineTo(20 * this.hp / this.type.hp, 0);
+	}
 
-    startIdle() {
-    	this.agro = null;
-    	switch (this.type.idle) {
-    	case "rabbit":
-    		this.action = rabbit(this, this.engine);
-    		break;
-    	case "patrol":
-    		this.action = patrol(this, this.engine);
-    		break;
-    	case "bounce":
-    		this.action = bounce(this, this.engine);
-    		break;
-    	case "hover":
-    		this.action = hover(this, this.engine);
-    		break;
-    	default:
-    		throw new Error("no idle for Enemy");
-    	}
-    }
+	startIdle() {
+		this.agro = null;
+		switch (this.type.idle) {
+		case "rabbit":
+			this.action = rabbit(this, this.engine);
+			break;
+		case "patrol":
+			this.action = patrol(this, this.engine);
+			break;
+		case "bounce":
+			this.action = bounce(this, this.engine);
+			break;
+		case "hover":
+			this.action = hover(this, this.engine);
+			break;
+		default:
+			throw new Error("no idle for Enemy");
+		}
+	}
 
-    startAgro(player: Player) {
-    	this.agro = player;
+	startAgro(player: Player) {
+		this.agro = player;
 
-    	switch (this.type.agro) {
-    	case "agro":
-    		this.action = agro(this, this.engine, player);
-    		break;
-    	case "heligun":
-    		this.action = heligun(this, this.engine, player);
-    		break;
-    	case "suicideBomber":
-    		this.action = suicideBomber(this, this.engine, player);
-    		break;
-    	default:
-    		throw new Error("no agro for Enemy");
-    	}
-    }
+		switch (this.type.agro) {
+		case "agro":
+			this.action = agro(this, this.engine, player);
+			break;
+		case "heligun":
+			this.action = heligun(this, this.engine, player);
+			break;
+		case "suicideBomber":
+			this.action = suicideBomber(this, this.engine, player);
+			break;
+		default:
+			throw new Error("no agro for Enemy");
+		}
+	}
 }
