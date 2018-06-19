97f8a3f87858cab796edcda266651b0f4c4f53b8
digi <james@digijin.com>
2018-06-20 01:18:40 +1000

    adding tabs

--- src/Menu/Outfitting/Detail.js
+++ src/Menu/Outfitting/Detail.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import { Text, Container } from "react-pixi-fiber";
 import Rectangle from "../Rectangle";
 import Line from "../Line";
+import Tab from "../Tab";
 import { UICOLOUR } from "../constants";
 import engineConnect from "React/engineConnect";
+
+import { Engines } from "Components/Engine";
+import { Legs } from "Components/Legs";
+import { Primarys } from "Components/Primary";
+import { Secondarys } from "Components/Secondary";
+import { Sidekicks } from "Components/Sidekick";
+import { Bodys } from "Components/Body";
+import { Boosters } from "Components/Booster";
 
 class OutfittingDetail extends Component {
 	render() {
@@ -110,6 +119,15 @@ class OutfittingDetail extends Component {
 					width={700}
 					height={210}
 					border={1}
+					borderColor={UICOLOUR}
+				/>
+				<Tab
+					x={591 + 10}
+					y={348}
+					width={75}
+					height={25}
+					border={1}
+					sideWidth={15}
 					borderColor={UICOLOUR}
 				/>
 			</Container>
---
+++ src/Menu/Tab.js
@@ -0,0 +1,61 @@
+import { CustomPIXIComponent } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
+
+const TYPE = "Tab";
+export const behavior = {
+	customDisplayObject: props => new PIXI.Graphics(),
+	customDidAttach: instance => {
+		// instance.buttonMode = true;
+		// instance.interactive = true;
+		instance.on("mouseover", instance.onMouseOver);
+		instance.on("mouseout", instance.onMouseOut);
+		instance.on("mouseup", instance.onMouseUp);
+		instance.on("mousedown", instance.onMouseDown);
+		instance.on("click", instance.onClick);
+	},
+	customApplyProps: function(instance, oldProps, newProps) {
+		instance.onMouseOver = newProps.onMouseOver;
+		instance.onMouseOut = newProps.onMouseOut;
+		instance.onMouseDown = newProps.onMouseDown;
+		instance.onMouseUp = newProps.onMouseUp;
+		instance.onClick = newProps.onClick;
+		let {
+			fill,
+			x,
+			y,
+			width,
+			height,
+			border,
+			borderColor,
+			alpha,
+			buttonMode,
+			interactive,
+			sideWidth
+		} = newProps;
+		this.cacheAsBitmap = false;
+		if (alpha == undefined) {
+			alpha = 1;
+		}
+		instance.clear();
+		if (fill !== undefined) {
+			instance.beginFill(fill, alpha);
+		}
+		instance.lineStyle(border, borderColor);
+
+		instance.moveTo(x, y);
+		instance.lineTo(x + sideWidth, y - height);
+		instance.lineTo(x + sideWidth + width, y - height);
+		instance.lineTo(x + sideWidth + width + sideWidth, y);
+
+		instance.endFill();
+
+		instance.buttonMode = buttonMode;
+		instance.interactive = interactive;
+		// console.log(instance);
+		// instance.mouseover = () => {
+		// 	console.log("yolo");
+		// };
+		this.cacheAsBitmap = true;
+	}
+};
+export default CustomPIXIComponent(behavior, TYPE);
