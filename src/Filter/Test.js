26e16d2ca7ffd45933d183fc35be3f4a7f46f6f1
digi <james@digijin.com>
2018-06-24 17:49:21 +1000

    checkerboard filter

--- src/Filter/Test.js
+++ src/Filter/Test.js
@@ -16,18 +16,31 @@ uniforms.time = {
 	type: "f",
 	value: 0
 };
-// uniforms.resolution = {
-//   type:"v2",
-//   value:{x:width,y:height}
-// }
+uniforms.resolution = {
+	type: "v2",
+	value: { x: window.innerWidth, y: window.innerHeight }
+};
 const TestFilter = new PIXI.Filter(
 	"",
 	`
 	precision mediump float;
+	varying vec2 vTextureCoord;
+	uniform sampler2D uSampler;
 	uniform vec2 mouse;
+	uniform vec2 resolution;
 	uniform float time;
 	void main(){
-		gl_FragColor = vec4(gl_FragCoord.x/mouse.x,gl_FragCoord.y/mouse.x,sin(time),1.0);
+		float blockWidth = 20.0;
+		float pc = abs(mod(time, 1.0));
+		// float pc = mouse.x/resolution.x;
+		float col = ceil(gl_FragCoord.x/blockWidth);
+		if(mod(gl_FragCoord.y + (col * blockWidth/2.0), blockWidth) < blockWidth * pc){
+			// gl_FragColor = vec4(0.0,0.0,0.0,0.0);
+			gl_FragColor = vec4(0.0,0.0,0.0,0.0);
+		}else{
+			// gl_FragColor = vec4(gl_FragCoord.x/mouse.x,gl_FragCoord.y/mouse.x,sin(time),1.0);
+			gl_FragColor = texture2D(uSampler, vTextureCoord);
+		}
 	}
 	`,
 	uniforms
@@ -36,7 +49,7 @@ const TestFilter = new PIXI.Filter(
 function animate() {
 	requestAnimationFrame(animate);
 	// uniforms.time.value += 0.1;
-	TestFilter.uniforms.time = TestFilter.uniforms.time + 0.01;
+	TestFilter.uniforms.time = TestFilter.uniforms.time + 0.04;
 	// console.log(TestFilter.uniforms.time);
 }
 animate();
