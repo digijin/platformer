f35124cff96e741138bb51b4e9fdc47f98bddc75
digi <james@digijin.com>
2018-06-24 12:07:25 +1000

    new colour

--- src/Menu/constants.js
+++ src/Menu/constants.js
@@ -1,1 +1,4 @@
-export const UICOLOUR = 0xff5000;
+// export const UICOLOUR = 0xedd883;
+// export const UICOLOUR = 0xeda883;
+// export const UICOLOUR = 0xff5000;
+export const UICOLOUR = 0xff9362;
