1d9acd36bd2721a5cdf6461357898e5266b9b67d
digi <james@digijin.com>
2018-06-24 19:48:25 +1000

    making it into a class

--- src/Filter/CheckerboardTransition.js
+++ src/Filter/CheckerboardTransition.js
@@ -10,32 +10,71 @@ import fragment from "./CheckerboardTransition/fragment.glsl";
 //https://github.com/pixijs/pixi.js/wiki/v4-Creating-Filters
 
 const uniforms = {};
-uniforms.mouse = {
-	type: "v2",
-	value: { x: 0, y: 0 }
-};
-uniforms.time = {
+// uniforms.mouse = {
+// 	type: "v2",
+// 	value: [400, 400]
+// };
+// uniforms.time = {
+// 	type: "f",
+// 	value: 0
+// };
+uniforms.percent = {
 	type: "f",
 	value: 0
 };
 uniforms.resolution = {
 	type: "v2",
-	value: { x: window.innerWidth, y: window.innerHeight }
+	value: [window.innerWidth, window.innerHeight]
 };
-const TestFilter = new PIXI.Filter("", fragment, uniforms);
 
-function animate() {
-	requestAnimationFrame(animate);
-	// uniforms.time.value += 0.1;
-	TestFilter.uniforms.time = TestFilter.uniforms.time + 0.04;
-	// console.log(TestFilter.uniforms.time);
+class CheckerboardTransitionFilter extends PIXI.Filter {
+	constructor() {
+		super("", fragment, uniforms);
+	}
+
+	get percent() {
+		return this.uniforms.percent;
+	}
+
+	set percent(val: number) {
+		this.uniforms.percent = val;
+	}
 }
-animate();
+
+const TestFilter = new CheckerboardTransitionFilter();
+
+// TestFilter.uniforms.mouse = [300, 300];
+
+// function animate() {
+// 	requestAnimationFrame(animate);
+// 	// uniforms.time.value += 0.1;
+// 	TestFilter.uniforms.time = TestFilter.uniforms.time + 0.04;
+// 	// console.log(TestFilter.uniforms.time);
+// }
+// animate();
 document.onmousemove = function(evt) {
 	const mousePos = { x: evt.clientX, y: evt.clientY };
 	// uniforms.mouse.value = mousePos;
-	TestFilter.uniforms.mouse = [mousePos.x, mousePos.y];
-	TestFilter.uniforms.resolution = [window.innerWidth, window.innerHeight];
+	// TestFilter.uniforms.mouse = [mousePos.x, mousePos.y];
+	console.log(TestFilter.uniforms.mouse);
+	// TestFilter.uniforms.mouse = [300, 300];
+	// TestFilter.uniforms.resolution = [window.innerWidth, window.innerHeight];
 };
 
 export default TestFilter;
+
+// ////////////////
+//
+// export default class CheckerboardTransitionFilter extends PIXI.Filter {
+// 	constructor() {
+// 		super("", fragment, uniforms);
+// 	}
+//
+// 	get percent() {
+// 		return this.uniforms.percent;
+// 	}
+//
+// 	set percent(val: number) {
+// 		this.uniforms.percent = val;
+// 	}
+// }
--- src/Filter/CheckerboardTransition/fragment.glsl
+++ src/Filter/CheckerboardTransition/fragment.glsl
@@ -1,13 +1,15 @@
 precision mediump float;
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
-uniform vec2 mouse;
+// uniform vec2 mouse;
 uniform vec2 resolution;
-uniform float time;
+// uniform float time;
+uniform float percent;
 void main(){
 	float blockWidth = 50.0;
 	// float pc = abs(mod(time, 1.0));
-	float pc = mouse.y/resolution.y * 2.0;
+	// float pc = mouse.y/resolution.y * 2.0;
+	float pc = percent * 2.0;
 	//invert
 	pc -= (resolution.y-gl_FragCoord.y) / resolution.y;
 	float col = ceil(gl_FragCoord.x/blockWidth);
