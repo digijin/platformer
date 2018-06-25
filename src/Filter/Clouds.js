1911d9ab714b0da60ca739adb374f418c5ef1894
digi <james@digijin.com>
2018-06-25 22:54:15 +1000

    trying to implement cloud shader

---
+++ src/Filter/Clouds.js
@@ -0,0 +1,32 @@
+import * as PIXI from "pixi.js";
+import fragment from "./Clouds_f.glsl";
+import cloudshader from "assets/cloudshader.png";
+
+const uniforms = {};
+
+uniforms.time = {
+	type: "f",
+	value: -1
+};
+uniforms.resolution = {
+	type: "v2",
+	value: [window.innerWidth, window.innerHeight]
+};
+uniforms.iChannel0 = {
+	type: "sampler2D",
+	value: new PIXI.Texture(new PIXI.BaseTexture(cloudshader))
+};
+
+export default class MenuBackgroundFilter extends PIXI.Filter {
+	constructor() {
+		super("", fragment, uniforms);
+	}
+
+	get time() {
+		return -this.uniforms.time;
+	}
+
+	set time(val: number) {
+		this.uniforms.time = -val;
+	}
+}
