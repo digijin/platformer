f9a155c1b6b580ad553be0091d44e3d766bd1e3f
digi <james@digijin.com>
2018-07-22 18:22:47 +1000

    thinking about bevel

--- src/Filter/Chrome/fragment_f.glsl
+++ src/Filter/Chrome/fragment_f.glsl
@@ -23,7 +23,25 @@ void main( )
 	vec3 colour = vec3(vTextureCoord.y)* base.a;
 	gl_FragColor = vec4(colour, base.a);
 	if(pixelCoord.y-40. <4.){
-		gl_FragColor = vec4(1.);
+		gl_FragColor = vec4(vec3(.8), 1.);
 	}
 	gl_FragColor = gl_FragColor * base.a;
+
+	for(int i = 0; i<3; i++)
+	{
+		vec2 offsetCoord = vec2(pixelCoord.x+float(i), pixelCoord.y)/filterArea.xy;
+		vec4 sample = texture2D(uSampler, offsetCoord);
+		if(sample.a<0.1 && base.a > 0.1){
+			gl_FragColor = vec4(1.,0.,0.,1.);
+			// gl_FragColor.x = mix(gl_FragColor.x, 1., 0.5);
+		}
+
+		// offsetCoord = vec2(pixelCoord.x-float(i), pixelCoord.y)/filterArea.xy;
+		// sample = texture2D(uSampler, offsetCoord);
+		// if(sample.a<0.1 && base.a > 0.1){
+		// 	gl_FragColor = vec4(1.,0.,0.,1.);
+		// 	// gl_FragColor.x = mix(gl_FragColor.x, 1., 0.5);
+		// }
+	}
+
 }
