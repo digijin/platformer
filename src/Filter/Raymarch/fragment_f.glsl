2b716a548227f1487ded8859cdd8737554a67fb2
digi <james@digijin.com>
2018-07-29 11:36:17 +1000

    multicoloured slices

--- src/Filter/Raymarch/fragment_f.glsl
+++ src/Filter/Raymarch/fragment_f.glsl
@@ -9,6 +9,8 @@ uniform vec2 iMouse;
 uniform float iTime;
 uniform vec4 filterArea;
 
+#define PI 3.1415926535897932384626433832795
+
 /**
  * Part 4 Challenges:
  * - Show the union instead of the intersection
@@ -51,15 +53,15 @@ float cubeSDF(vec3 p) {
     // If d.x < 0, then -1 < p.x < 1, and same logic applies to p.y, p.z
     // So if all components of d are negative, then p is inside the unit cube
     vec3 d = abs(p) - vec3(1.0, 1.0, 1.0);
-    
+
     // Assuming p is inside the cube, how far is it from the surface?
     // Result will be negative or zero.
     float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
-    
+
     // Assuming p is outside the cube, how far is it from the surface?
     // Result will be positive or zero.
     float outsideDistance = length(max(d, 0.0));
-    
+
     return insideDistance + outsideDistance;
 }
 
@@ -98,50 +100,68 @@ float sphericalSliceSDF(vec3 p, float s1, float s2){
 
 /**
  * Signed distance function describing the scene.
- * 
+ *
  * Absolute value of the return value indicates the distance to the surface.
  * Sign indicates whether the point is inside or outside the surface,
  * negative indicating inside.
  */
-float sceneSDF(vec3 samplePoint) {
+vec4 sceneSDF(vec3 samplePoint) {
     // float sphereDist = sphereSDF(samplePoint / 1.2) * 1.2;
     // float sphereCapDist = sphericalCapSDF(samplePoint / 1.2, sin(iTime)) * 1.2;
     // float cubeDist = cubeSDF(samplePoint);
     // return intersectSDF(cubeDist, sphereDist);
 	// return intersectSDF(sphereDist, sphereCapDist);
 	// return sphereCapDist;
-	return sphericalSliceSDF(samplePoint, sin(iTime), cos(iTime));
+	float pc = mod(iTime/10., 1.)*4.-1.;
+	// float sliceB = sphericalSliceSDF(samplePoint, sin(iTime+PI), cos(iTime+PI));
+	float sliceA = sphericalSliceSDF(samplePoint, 1., pc-1.);
+	float sliceB = sphericalSliceSDF(samplePoint, pc-1., pc-2.);
+	float sliceC = sphericalSliceSDF(samplePoint, pc-2., pc-3.);
+	float sliceD = sphericalSliceSDF(samplePoint, pc-3., pc-4.);
+	//yellow red dark light
+	if(sliceA < sliceB){
+		return vec4( vec3(1.,1.,0.),sliceA);
+	}else if(sliceB < sliceC){
+		return vec4( vec3(1.,0.,0.),sliceB);
+	}else if(sliceC < sliceD){
+		return vec4( vec3(0.4),sliceC);
+	}else{
+		return vec4( vec3(0.8),sliceD);
+	}
 }
 
 /**
  * Return the shortest distance from the eyepoint to the scene surface along
  * the marching direction. If no part of the surface is found between start and end,
  * return end.
- * 
+ *
  * eye: the eye point, acting as the origin of the ray
  * marchingDirection: the normalized direction to march in
  * start: the starting distance away from the eye
  * end: the max distance away from the ey to march before giving up
  */
-float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
+vec4 shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
     float depth = start;
     for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
-        float dist = sceneSDF(eye + depth * marchingDirection);
+		vec4 data = sceneSDF(eye + depth * marchingDirection);
+        float dist = data.a;
         if (dist < EPSILON) {
-			return depth;
+			// return depth;
+			return vec4(data.xyz,depth);
         }
         depth += dist;
         if (depth >= end) {
-            return end;
+            // return end;
+			return vec4(data.xyz,end);
         }
     }
-    return end;
+    return vec4(vec3(1.),end);
 }
-            
+
 
 /**
  * Return the normalized direction to march in from the eye point for a single pixel.
- * 
+ *
  * fieldOfView: vertical field of view in degrees
  * size: resolution of the output image
  * fragCoord: the x,y coordinate of the pixel in the output image
@@ -157,15 +177,15 @@ vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
  */
 vec3 estimateNormal(vec3 p) {
     return normalize(vec3(
-        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)),
-        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)),
-        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON))
+        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)).a - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)).a,
+        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)).a - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)).a,
+        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)).a - sceneSDF(vec3(p.x, p.y, p.z - EPSILON)).a
     ));
 }
 
 /**
  * Lighting contribution of a single point light source via Phong illumination.
- * 
+ *
  * The vec3 returned is the RGB color of the light's contribution.
  *
  * k_a: Ambient color
@@ -185,15 +205,15 @@ vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
     vec3 L = normalize(lightPos - p);
     vec3 V = normalize(eye - p);
     vec3 R = normalize(reflect(-L, N));
-    
+
     float dotLN = dot(L, N);
     float dotRV = dot(R, V);
-    
+
     if (dotLN < 0.0) {
         // Light not visible from this point on the surface
         return vec3(0.0, 0.0, 0.0);
-    } 
-    
+    }
+
     if (dotRV < 0.0) {
         // Light reflection in opposite direction as viewer, apply only diffuse
         // component
@@ -204,7 +224,7 @@ vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
 
 /**
  * Lighting via Phong illumination.
- * 
+ *
  * The vec3 returned is the RGB color of that point after lighting is applied.
  * k_a: Ambient color
  * k_d: Diffuse color
@@ -218,24 +238,24 @@ vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
 vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
     const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
     vec3 color = ambientLight * k_a;
-    
+
     vec3 light1Pos = vec3(4.0 * sin(iTime),
                           2.0,
                           4.0 * cos(iTime));
     vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
-    
+
     color += phongContribForLight(k_d, k_s, alpha, p, eye,
                                   light1Pos,
                                   light1Intensity);
-    
+
     vec3 light2Pos = vec3(2.0 * sin(0.37 * iTime),
                           2.0 * cos(0.37 * iTime),
                           2.0);
     vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
-    
+
     color += phongContribForLight(k_d, k_s, alpha, p, eye,
                                   light2Pos,
-                                  light2Intensity);    
+                                  light2Intensity);
     return color;
 }
 
@@ -264,43 +284,46 @@ void main( )
 	// vec3 dir = rayDirection(45.0, iResolution.xy, gl_FragCoord.xy);
     // vec3 eye = vec3(0.0, 0.0, 15.0);
     // float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
-    
+
     // if (dist > MAX_DIST - EPSILON) {
     //     // Didn't hit anything
     //     gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 	// 	return;
     // }
-    
+
     // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-	
+
 	////
 	vec3 viewDir = rayDirection(45.0, iResolution.xy, gl_FragCoord.xy);
-    vec3 eye = vec3(8.0, 5.0, 7.0);
-    
+	vec2 pos =  iMouse/iResolution.xy;
+    vec3 eye = vec3(10.0* pos.x, 5.0*pos.y, 7.0);
+
     mat4 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
-    
+
     vec3 worldDir = (viewToWorld * vec4(viewDir, 0.0)).xyz;
-    
-    float dist = shortestDistanceToSurface(eye, worldDir, MIN_DIST, MAX_DIST);
-    
+
+	vec4 data = shortestDistanceToSurface(eye, worldDir, MIN_DIST, MAX_DIST);
+    float dist = data.a;
+
     if (dist > MAX_DIST - EPSILON) {
         // Didn't hit anything
         gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 		return;
     }
-    
+
     // The closest point on the surface to the eyepoint along the view ray
     vec3 p = eye + dist * worldDir;
-    
+
     vec3 K_a = vec3(0.2, 0.2, 0.2);
     vec3 K_d = vec3(0.7, 0.2, 0.2);
     vec3 K_s = vec3(1.0, 1.0, 1.0);
     float shininess = 10.0;
-    
-    vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
-    
-    gl_FragColor = vec4(color, 1.0);
 
+    vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
+
+    // gl_FragColor = vec4(color, 1.0);
+
+    gl_FragColor = vec4(data.rgb, 1.0);
 
 
 }
