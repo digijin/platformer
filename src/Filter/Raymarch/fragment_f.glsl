576395e38b2685e3cc83450a030f1456c7c02295
digi <james@digijin.com>
2018-08-05 15:00:46 +1000

    more explosiony fragments

--- src/Filter/Raymarch/fragment_f.glsl
+++ src/Filter/Raymarch/fragment_f.glsl
@@ -1,6 +1,7 @@
 
 
 //https://raw.githubusercontent.com/darrenmothersele/raymarch/master/shaders/frag.glsl
+//http://iquilezles.org/www/articles/distfunctions/distfunctions.htm
 
 #pragma glslify: smin = require(glsl-smooth-min)
 #pragma glslify: combineChamfer = require(glsl-combine-chamfer)
@@ -29,12 +30,49 @@ uniform vec4 filterArea;
 
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
-const float MAX_DIST = 100.0;
+const float MAX_DIST = 200.0;
 const float EPSILON = 0.0001;
 
 //golden ratio angle
 const float PHI = 137.5 * PI / 180.;
 
+
+float sdCone( in vec3 p, in vec3 c )
+{
+    vec2 q = vec2( length(p.xz), p.y );
+    float d1 = -q.y-c.z;
+    float d2 = max( dot(q,c.xy), q.y);
+    return length(max(vec2(d1,d2),0.0)) + min(max(d1,d2), 0.);
+}
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+    vec3 pa = p - a, ba = b - a;
+    float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+    return length( pa - ba*h ) - r;
+}
+
+mat4 rotationMatrix(vec3 axis, float angle)
+{
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
+
+mat3 rotAxis(vec3 axis, float a) {
+	float s=sin(a);
+	float c=cos(a);
+	float oc=1.0-c;
+	vec3 as=axis*s;
+	mat3 p=mat3(axis.x*axis,axis.y*axis,axis.z*axis);
+	mat3 q=mat3(c,-as.z,as.y,as.z,c,-as.x,-as.y,as.x,c);
+	return p*oc+q;
+}
 
 /**
  * Signed distance function describing the scene.
@@ -43,27 +81,39 @@ const float PHI = 137.5 * PI / 180.;
  * Sign indicates whether the point is inside or outside the surface,
  * negative indicating inside.
  */
+const int NUM_SPHERES = 10;
 float sceneSDF(vec3 samplePoint) {
 	float sphereDist = MAX_DIST;
 	// float sphere1 = sphereSDF(samplePoint, vec3(0.), 1.);
 	// float sphere2 = sphereSDF(samplePoint, vec3(sin(iTime*.75), cos(iTime*.98), sin(iTime*1.2)), 1.+cos(iTime*2.8)/4.);
 
 	// float sphereDist = smin(sphere1, sphere2, 0.1);// + snoise4(vec4(samplePoint, iTime))*0.1;
-
+	mat3 rot = rotAxis(normalize(vec3(1.,1.6,0.)), iTime);
 	// float sphereDist = sphere1;
+	// sphereDist = sdCone((samplePoint+ vec3(0.,0.,2.))*rot, vec3(1.,1.,1.));
+	//+ vec3(0.,0.,0.)
+	float pc = fract(iTime/4.);
+	vec3 start = vec3(4.) * ease(smoothstep(.3,1.,pc));
+	vec3 end = vec3(4.) * ease(smoothstep(.1,.8,pc));
+	sphereDist = sdCapsule(samplePoint, end, start, sin(pc*PI)*.2);
 
-	for(int i = 0; i<10; i++){
+
+	for(int i = 0; i<NUM_SPHERES; i++){
 		float angle = PHI * float(i);
 
 		// float sphere = sphereSDF(samplePoint, vec3(sin(iTime*.75+float(i)), cos(iTime*.98+float(i)), sin(iTime*1.2+float(i))), 1.);// + bumps*.1;
 		vec3 pos = vec3(sin(angle), cos(angle), 0.)*float(i)/4.;
 		float size = 1. + float(i)/10.;
 		// size *= fract(iTime/10.);
-		float pc = ease(smoothstep((float(i))/40., 1., fract(iTime/4.)*2.) *3.);
-		size *= pc;
-		pos *=pc;
-		float sphere = sphereSDF(samplePoint, pos, size);
-		sphereDist = smin(sphereDist, sphere, 0.2);
+		float phase = ease(smoothstep(float(i)/40., 1., pc*2.));
+		float outphase = ease(smoothstep(float(i)/10., 2., pc*2.));
+		// size *= phase;
+		// pos *=phase;
+		float sphere = sphereSDF(samplePoint, pos*phase, size*phase);
+		float outsphere = sphereSDF(samplePoint, pos*outphase, size*outphase*1.1);
+		// sphere = min(sphere,outsphere);
+		sphere = max(sphere,-outsphere);
+		sphereDist = min(sphereDist, sphere);
 	}
 
 	return sphereDist+  0.1*snoise3(samplePoint+vec3(0., iTime/2., 0.));
@@ -79,7 +129,7 @@ void main( )
 
 	vec3 viewDir = rayDirection(45.0, iResolution.xy, gl_FragCoord.xy);
 	vec2 pos =  ((iMouse/iResolution.xy) - .5)*8.;
-    vec3 eye = vec3(40.0* sin(pos.x), 0.0, 40.0*cos(pos.x));
+    vec3 eye = vec3(40.0* sin(pos.x), pos.y, 40.0*cos(pos.x));
     mat4 viewToWorld = lookAtMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
     vec3 worldDir = (viewToWorld * vec4(viewDir, 0.0)).xyz;
     float dist = shortestDistanceToSurface(eye, worldDir, MIN_DIST, MAX_DIST);
@@ -91,22 +141,9 @@ void main( )
 
     // The closest point on the surface to the eyepoint along the view ray
     vec3 p = eye + dist * worldDir;
-	//
-    // vec3 K_a = vec3(0.2, 0.2, 0.2);
-    // vec3 K_d = vec3(0.7, 0.2, 0.2);
-    // vec3 K_s = vec3(1.0, 1.0, 1.0);
-    // float shininess = 10.0;
-	//
-    // vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
-	//
-    // gl_FragColor = vec4(color, 1.0);
-	vec3 normal = estimateNormal(p);
-	//translate from world to camera;
-	// normal = worldDir-normal;
-	// gl_FragColor = vec4(normal, 1.0);
-	// vec3 screenNormal = vec3(length(worldDir-normal)/2.);
 
-	// gl_FragColor = vec4(normal, 1.);
+	vec3 normal = estimateNormal(p);
+
 
 	float a = 1.8;
 	float b = 1.99;
