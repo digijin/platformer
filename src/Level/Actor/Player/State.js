c409335ec5a63548cc7e22dc982d6c03a00f6938
digi <james@digijin.com>
2018-08-21 21:08:52 +1000

    actor state

---
+++ src/Level/Actor/Player/State.js
@@ -0,0 +1,10 @@
+
+export const GROUNDED = "grounded";
+export const AIRBORNE = "airborne";
+export const CLIMB = "climing";
+export const DASH = "dashing";
+
+
+const State = { GROUNDED, AIRBORNE, CLIMB, DASH };
+
+export default State;
