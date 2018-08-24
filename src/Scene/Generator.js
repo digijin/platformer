2083e2870d18e0e716db27cc3de808c4650cbf9d
digi <james@digijin.com>
2018-08-25 00:39:12 +1000

    room chain

--- src/Scene/Generator.js
+++ src/Scene/Generator.js
@@ -5,6 +5,7 @@ import type Engine from "Engine";
 
 import Grid3 from "Utility/Grid3";
 
+import GameObject from "GameObject";
 
 const GRIDSIZE = 4;
 const GRID_WIDTH = 200;
@@ -23,6 +24,103 @@ const MAX_BUILDING_WIDTH = 20;
 const MAX_FLOORS = 8;
 const FLOOR_HEIGHT = 5;
 
+
+const TILE_SIZE = 10;
+const NUM_CHILDREN = 20;
+class Generator extends GameObject {
+	init(engine){
+		super.init(engine);
+		this.gen = generateDungeon(engine);
+	}
+
+	update(){
+		// console.log(this.gen.next());
+		const result = this.gen.next();
+		console.log("generator", result.value);
+		if(result.done){
+			this.destroy();
+		}
+	}
+}
+const generateDungeon = function*(engine){
+	const container = new PIXI.Container();
+	engine.stage.addChild(container);
+	// container.position.x = window.innerWidth / 2;
+	container.position.y = window.innerHeight / 2;
+	// const children = [];
+	for(let i = 0; i < NUM_CHILDREN; i++){
+		const sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+		sprite.width = Math.ceil(Math.random() * 10) * TILE_SIZE;
+		sprite.height = Math.ceil(Math.random() * 10) * TILE_SIZE;
+		sprite.position.x = Math.ceil(Math.random() * 10 ) * TILE_SIZE;
+		sprite.position.y = Math.ceil(Math.random() * 10 ) * TILE_SIZE;
+		sprite.tint = Math.ceil(Math.random() * 0xffffff);
+		container.addChild(sprite);
+		// children.push(sprite);
+		yield i;
+	}
+	container.children.sort((a, b) => {
+		//centers
+		const cA = {
+			x: a.position.x + (a.width / 2),
+			y: a.position.y + (a.height / 2),
+		};
+		const cB = {
+			x: b.position.x + (b.width / 2),
+			y: b.position.y + (b.height / 2),
+		};
+		//a2+b2=c2
+		const distAsq = (cA.x * cA.x) + (cA.y * cA.y);
+		const distBsq = (cB.x * cB.x) + (cB.y * cB.y);
+
+		return   distBsq - distAsq;
+	});
+
+	const children = container.children.slice(0).reverse();
+
+	for(let i = 0; i < children.length; i++){
+		const child = children[i];
+		//move all other overlapping away
+		const childRect = {
+			t: child.position.y,
+			l: child.position.x,
+			b: child.position.y + child.height,
+			r: child.position.x + child.width,
+		};
+		for(let j = i + 1; j < children.length; j++){
+			const other = children[j];
+			const otherRect = {
+				t: other.position.y,
+				l: other.position.x,
+				b: other.position.y + other.height,
+				r: other.position.x + other.width,
+			};
+
+			const rOver = childRect.r > otherRect.l;
+			const bOver = childRect.b > otherRect.t;
+			const tOver = childRect.t < otherRect.b;
+			const lOver = childRect.l < otherRect.r;
+			if(rOver && bOver){
+				if(rOver ){
+					other.position.x += childRect.r - otherRect.l;
+				}else if(bOver ){
+					other.position.y += childRect.b - otherRect.t;
+				}
+			}
+			// if(rOver && bOver  && tOver && lOver){
+			// 	if(rOver && !lOver){
+			// 		other.position.x += childRect.r - otherRect.l;
+			// 	}
+			// 	if(bOver && !tOver){
+			// 		other.position.y += childRect.b - otherRect.t;
+			// 	}
+			// }
+			yield [i, j];
+
+		}
+	}
+};
+
 export default class GeneratorManager extends Base {
 	start(engine: Engine) {
 		super.start(engine);
@@ -30,7 +128,10 @@ export default class GeneratorManager extends Base {
 		this.grid = new Grid3(GRID_WIDTH, GRID_HEIGHT, 1, Block);
 		this.generate();
 		this.draw();
+		// this.gen = generateDungeon();
+		engine.register(new Generator());
 	}
+
 
 	generate(){
 		// this.grid.row(GROUND).forEach(cell => cell.type = 1);
@@ -38,8 +139,8 @@ export default class GeneratorManager extends Base {
 		this.genGround();
 		while(x < GRID_WIDTH){
 			const spacing = MIN_BUILDING_SPACING + Math.ceil((MAX_BUILDING_SPACING - MIN_BUILDING_SPACING) * Math.random());
-			x += spacing;
-			this.genTunnel(x, GROUND);
+			// x += spacing;
+			// this.genTunnel(x, GROUND);
 			x += spacing;
 			x += this.genBuilding(x, GROUND);
 
@@ -55,7 +156,6 @@ export default class GeneratorManager extends Base {
 	genTunnel(xOff, yOff){
 		const x = xOff;
 		for(let y = yOff; y < yOff + 10; y++){
-
 			this.grid[x][y][0].type = 0;
 		}
 		// this.grid[xOff][yOff + 10][0].type = 0;
@@ -97,3 +197,5 @@ export default class GeneratorManager extends Base {
         
 	}
 }
+
+
