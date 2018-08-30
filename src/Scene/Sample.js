1c4c8d4aa054d0728a8ac8bf228710cbdaa2416d
digi <james@digijin.com>
2018-08-30 23:19:56 +1000

    sample screen

---
+++ src/Scene/Sample.js
@@ -0,0 +1,28 @@
+import Base from "./Base";
+import * as PIXI from "pixi.js";
+import type Engine from "Engine";
+import GameObject from "GameObject";
+
+
+class Looper extends GameObject {
+	init(engine){
+		super.init(engine);
+	}
+
+	update(){
+		
+	}
+}
+
+export default class GeneratorManager extends Base {
+	start(engine: Engine) {
+		super.start(engine);
+		document.body.style.backgroundColor = "red";
+		this.container = new PIXI.Container();
+		this.engine.stage.addChild(this.container);
+		engine.register(new Looper(this));
+	}
+
+}
+
+
