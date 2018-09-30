ab59f183e0665aa99c1a9cf4e3c1ff4b61c340f6
digi <james@digijin.com>
2018-10-01 00:06:39 +1000

    clouds

--- src/Filter/Buildings/fragment_f.glsl
+++ src/Filter/Buildings/fragment_f.glsl
@@ -32,6 +32,10 @@ uniform vec4 filterArea;
 
 #define PI 3.1415926535897932384626433832795
 
+const bool TRANSPARENT = true;
+
+
+
 const int MAX_MARCHING_STEPS = 256;
 const float MIN_DIST = 0.0;
 const float MAX_DIST = 1000.0;
@@ -75,28 +79,54 @@ float outerSDF(vec3 samplePoint){
 	);
 	return -boxSDF(point, vec3(4.,10.,4.));
 }
-float raymarch(vec3 eye, vec3 marchingDirection, float start, float end) {
+
+const float DEPTH_RATIO = .5;
+const float DEPTH_MAX_STEP = 1.;
+
+float cloudnoise(vec3 point){
+
+	float topfadeout = smoothstep(10.,5., point.y);
+	float noise = max(snoise3(point/5.), 0.);
+	
+	// if(point.y>5.){
+	// 	return 0.;
+	// }
+
+	return mix(0., noise, topfadeout);
+}
+
+vec2 raymarch(vec3 eye, vec3 marchingDirection, float start, float end) {
     float depth = start;
 	float dist = 0.;
+	float cloud = 0.;
     for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
 		vec3 point = eye + depth * marchingDirection;
 		dist = sceneSDF(point);
 		if (dist < EPSILON) {
-			return depth;
+			return vec2(depth, cloud);
 		}
 		// float outer = outerSDF(point);
 		// if(outer < 0.){
 		// 	outer = 10.;
 		// }
-        depth += min(dist * .3, 1.);
+		if(dist * DEPTH_RATIO > DEPTH_MAX_STEP){
+			cloud += cloudnoise(point);
+			depth += DEPTH_MAX_STEP;
+		}else{
+			cloud += cloudnoise(point) * dist * DEPTH_RATIO / DEPTH_MAX_STEP;
+        	depth += dist * DEPTH_RATIO;
+		}
+		// cloud = max(cloud, cloudnoise(point));
+
+
         if (depth >= end) {
-			return end;
+			return vec2(end, cloud);
         }
     }
 	if (dist < EPSILON) {
-		return depth;
+		return vec2(depth, cloud);
 	}else{
-    	return end;
+    	return vec2(end, cloud);
 	}
 }
 #pragma glslify: estimateNormal = require(../utility/estimateNormal_f.glsl,sceneSDF=sceneSDF)
@@ -108,7 +138,9 @@ void main( )
 	vec3 eye = iPosition*10. + (vec3(2.,0.,-1.)*iTime*1.);
     mat4 viewToWorld = lookAtMatrix(eye, eye+ iRotation, vec3(0.0, 1.0, 0.0));
     vec3 worldDir = (viewToWorld * vec4(viewDir, 0.0)).xyz;
-	float dist = raymarch(eye, worldDir, MIN_DIST, MAX_DIST);
+	vec2 result = raymarch(eye, worldDir, MIN_DIST, MAX_DIST); 
+	float dist = result.x;
+	float cloud = result.y /10.;
     if (dist > MAX_DIST - EPSILON) {
         // Didn't hit anything
         // gl_FragColor = vec4(worldDir, 0.0);
@@ -125,11 +157,13 @@ void main( )
 	vec3 tint = vec3(255., 147., 98.) /255.;
     // gl_FragColor = vec4(normal, 1.0);
 	float degree = (normal.x/2.) + (normal.y/4.) + (normal.z/8.) + (1./8.);
-	// gl_FragColor = vec4(degree*tint, 1.0);
-	gl_FragColor = vec4(vec3(0.), degree);
 
-	float fadein = smoothstep(0.,2.,iTime);
-	gl_FragColor = mix(vec4(0.), gl_FragColor, fadein);
+	if(TRANSPARENT){
+		gl_FragColor = vec4(vec3(0.), degree - cloud);
+	}else{
+		gl_FragColor = vec4(degree*tint, 1.0);
+	}
+
 	
 	//WINDOWS
 	// if(fract(p.x)>0.1 && fract(p.x)<0.9){
@@ -141,7 +175,12 @@ void main( )
 	// 	}
 	// }
 
+	//cloud
+	// gl_FragColor.r = cloud / 10.;
 
+	//FADE IN
+	float fadein = smoothstep(0.,2.,iTime);
+	gl_FragColor = mix(vec4(0.), gl_FragColor, fadein);
 	// float power = light(
 	// 	normalize(lightpos-p),
 	// 	normalize(worldDir),
