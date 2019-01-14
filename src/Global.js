50879202363f4bbb4bd7c025701c1f3136408f54
digi <james@digijin.com>
2019-01-14 18:08:37 +1100

    global class

--- src/Global.js
+++ src/Global.js
@@ -3,13 +3,21 @@
 class Global{
 
     values = {}
+    valuePromises = {}
 
     get(key){
     	const value = this.values[key];
     	const prom = new Promise((resolve, reject)=>{
     		if(value){
     			resolve(value);
+    		}else{
+
+    			if (!this.valuePromises[key]){
+    				this.valuePromises[key] = [];
+    			}
+    			this.valuePromises[key].push(resolve);
     		}
+            
     	});
     	
     	return prom;
@@ -18,6 +26,19 @@ class Global{
 
     set(key, value){
     	this.values[key] = value;
+    	if(this.valuePromises[key]){
+    		this.valuePromises[key].forEach(resolve => {
+    			resolve(value);
+    		});
+
+
+    		this.valuePromises[key] = [];
+    	}
+    }
+
+    clear(){
+    	this.values = {};
+    	this.valuePromises = {};
     }
 }
 
--- src/Global.spec.js
+++ src/Global.spec.js
@@ -2,6 +2,9 @@
 import Global from "./Global";
 
 describe("Global", () => {
+	beforeEach(() => {
+		Global.clear();
+	});
 	describe("get", () => {
 		it("should return a promise", () => {
 			expect(Global.get("key").toString()).toBe("[object Promise]");
@@ -18,9 +21,15 @@ describe("Global", () => {
 	});
 	describe("set", () => {
 		it("should set a key", () =>{
-			Global.values = {};
 			Global.set("key", "123");
 			expect(Global.values["key"]).toBe("123");
+		});
+		it("should notify other promises", (done) => {
+			Global.get("key").then((val) => {
+				expect(val).toBe("abc123");
+				done();
+			});
+			Global.set("key", "abc123");
 		});
         
 
