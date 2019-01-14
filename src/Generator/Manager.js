0dad74adbd977b48589697d6ff260175548c323f
digi <james@digijin.com>
2019-01-14 17:29:15 +1100

    generate faster

--- src/Generator/Manager.js
+++ src/Generator/Manager.js
@@ -15,11 +15,15 @@ class GeneratorManager extends GameObject {
 	}
 
 	update(){
-		const result = this.gen.next();
-		if(result.done){
-			this.destroy();
-			this.engine.stage.removeChild(this.manager.container);
-			this.engine.startScene(new Level());
+		let result = this.gen.next();
+		// let start = new Date().getTime();
+		for(let x = 0; x < 10; x++){
+			if(result.done){
+				this.destroy();
+				this.engine.stage.removeChild(this.manager.container);
+				this.engine.startScene(new Level());
+			}
+			result = this.gen.next();
 		}
 	}
 }
