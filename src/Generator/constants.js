29467f3fd7fb3520271800a1e89641355cf75286
digi <james@digijin.com>
2019-08-22 22:05:02 +1000

    bigger buildings

--- src/Generator/constants.js
+++ src/Generator/constants.js
@@ -1,8 +1,6 @@
-
 export const GRIDSIZE = 4;
 export const GRID_WIDTH = 400;
 export const GRID_HEIGHT = 200;
-
 
 export const GROUND = 100;
 export const MIN_BUILDING_SPACING = 4;
@@ -14,8 +12,9 @@ export const MAX_BUILDING_SHAFT_WIDTH = 10;
 export const BUILDING_WALL_CHANCE = .02;
 export const BUILDING_SIDE_WALL_CHANCE = .5;
 export const BUILDING_SHAFT_CHANCE = .05;
-export const MAX_FLOORS = 16;
-export const FLOOR_HEIGHT = 6;
+export const MAX_FLOORS = 10;
+export const FLOOR_HEIGHT = 7;
 export const TILE_SIZE = 4;
 export const NUM_CHILDREN = 30;
 export const ENEMIES = true;
+export const DOOR_HEIGHT = 3;
--- src/Generator/generateBuildings.js
+++ src/Generator/generateBuildings.js
@@ -1,11 +1,25 @@
+import {
+	BUILDING_SHAFT_CHANCE,
+	BUILDING_SIDE_WALL_CHANCE,
+	BUILDING_WALL_CHANCE,
+	DOOR_HEIGHT,
+	ENEMIES,
+	FLOOR_HEIGHT,
+	GRID_WIDTH,
+	GROUND,
+	MAX_BUILDING_SHAFT_WIDTH,
+	MAX_BUILDING_SPACING,
+	MAX_BUILDING_WIDTH,
+	MAX_FLOORS,
+	MIN_BUILDING_SHAFT_WIDTH,
+	MIN_BUILDING_SPACING,
+	MIN_BUILDING_WIDTH,
+} from "./constants";
 
-import { MAX_FLOORS, GRID_WIDTH, FLOOR_HEIGHT, MIN_BUILDING_SPACING, MAX_BUILDING_SPACING, GROUND, MAX_BUILDING_WIDTH, MIN_BUILDING_WIDTH, ENEMIES, BUILDING_WALL_CHANCE, MIN_BUILDING_SHAFT_WIDTH, MAX_BUILDING_SHAFT_WIDTH,  BUILDING_SHAFT_CHANCE, BUILDING_SIDE_WALL_CHANCE } from "./constants";
-
-
-export default function* (manager, grid){
+export default function* (manager, grid) {
 	// console.log("genbuildings", manager);
 	let x = 0;
-	while(x < GRID_WIDTH){
+	while (x < GRID_WIDTH) {
 		const spacing = MIN_BUILDING_SPACING + Math.ceil((MAX_BUILDING_SPACING - MIN_BUILDING_SPACING) * Math.random());
 		// x += spacing;
 		// this.genTunnel(x, GROUND);
@@ -17,25 +31,25 @@ export default function* (manager, grid){
 }
 
 
-function *genBuilding(xOff, yOff, manager, grid){
+function* genBuilding(xOff, yOff, manager, grid) {
 	// console.log("genbuilding", manager);
 	const width = MIN_BUILDING_WIDTH + Math.ceil((MAX_BUILDING_WIDTH - MIN_BUILDING_WIDTH) * Math.random());
 	const floors = Math.ceil(MAX_FLOORS * Math.random());
 
 	///////////////////ENEMIES
 	//hovering helicopters
-	for(let i = 0; i < 1; i++){
+	for (let i = 0; i < 1; i++) {
 		const block = manager.grid.get(xOff + Math.floor(Math.random() * width), yOff - ((FLOOR_HEIGHT) * (floors + 2 + (i * 2))));
-		if(ENEMIES){
+		if (ENEMIES) {
 			grid.addEnemyData({ block, type: { id: "4" } });
 		}
 
 	}
 	//plant some dudes on the floors
-	for(let i = 0; i < floors; i++){
-		if(Math.random() > 0.9){
+	for (let i = 0; i < floors; i++) {
+		if (Math.random() > 0.9) {
 			const block = manager.grid.get(xOff + Math.floor(Math.random() * width), yOff - ((FLOOR_HEIGHT) * i));
-			if(ENEMIES){
+			if (ENEMIES) {
 				grid.addEnemyData({ block, type: { id: "3" } });
 			}
 		}
@@ -43,52 +57,52 @@ function *genBuilding(xOff, yOff, manager, grid){
 	//////////////////END ENEMIES
 
 	// actual buildings
-	for(let f = 1; f <= floors; f++){
+	for (let f = 1; f <= floors; f++) {
 		let block;
 		let shaft = 0;
-		for(let x = xOff; x < xOff + width; x++){
-		// console.log(wall);
-			
+		for (let x = xOff; x < xOff + width; x++) {
+			// console.log(wall);
+
 			let wall = Math.random() < BUILDING_WALL_CHANCE;
 			const y = yOff - (FLOOR_HEIGHT * f);
 
 			//SIDES
-			if(x == xOff || x == xOff + width - 1){
+			if (x === xOff || x === xOff + width - 1) {
 				wall = wall || Math.random() < BUILDING_SIDE_WALL_CHANCE;
 			}
 			//WALL
-			if(wall){
+			if (wall) {
 				// console.log("triggers", FLOOR_HEIGHT);
-				for(let j = 0; j < FLOOR_HEIGHT - 2; j++){
+				for (let j = 0; j < FLOOR_HEIGHT - DOOR_HEIGHT; j++) {
 					// console.log(x, y - j);
-					block = manager.grid.get(x, y + j );
-					if(block){
+					block = manager.grid.get(x, y + j);
+					if (block) {
 						block.type = "1";
 					}
 				}
 			}
 
 			//FLOOR
-			if(shaft == 0){
-				if(Math.random() < BUILDING_SHAFT_CHANCE){
+			if (shaft === 0) {
+				if (Math.random() < BUILDING_SHAFT_CHANCE) {
 					shaft = MIN_BUILDING_SHAFT_WIDTH + Math.ceil(Math.random() * (MAX_BUILDING_SHAFT_WIDTH - MIN_BUILDING_SHAFT_WIDTH));
 				}
 			}
 
 			block = manager.grid.get(x, y);
-			if(block){
-				if(shaft > 0){
+			if (block) {
+				if (shaft > 0) {
 
 					block.type = "platform";
-					shaft --;
-				}else{
+					shaft--;
+				} else {
 					block.type = "1";
 				}
 
 			}
 		}
 
-		
+
 		// for(let y = yOff - (FLOOR_HEIGHT * floors); y < yOff; y++){
 		// 	const block  = manager.grid.get(x, y);
 		// 	if(block){
