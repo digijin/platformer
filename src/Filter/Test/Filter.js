0718c07173b63ebb2e92be14ad20e1fea65b6da9
digi <james@digijin.com>
2019-09-15 13:34:42 +1000

    shaders working again

--- src/Filter/Test/Filter.js
+++ src/Filter/Test/Filter.js
@@ -3,38 +3,19 @@ import fragment from "./fragment_f.glsl";
 // import texture from "./texture.png";
 import texture from "assets/mech.png";
 
+import log from "loglevel";
+
 const uniforms = {};
 
-uniforms.iTime = {
-	type: "f",
-	value: -1,
-};
-uniforms.iMouse = {
-	type: "v2",
-	value: [0, 0],
-};
-uniforms.iResolution = {
-	type: "v2",
-	value: [window.innerWidth, window.innerHeight],
-};
-uniforms.iChannel0 = {
-	type: "sampler2D",
-	value: new PIXI.Texture(new PIXI.BaseTexture(texture)),
-};
-// uniforms.iChannel0 = {
-// 	type: "sampler2D",
-// 	value: new PIXI.Texture(new PIXI.BaseTexture(texture))
-// };
-// uniforms.seeds = {
-// 	type: "v2v",
-// 	// value: [0.123, 0.321, 0.456, 0.654, 0.789, 0.987]
-// 	value: new Array(16).fill(0).map(() => {
-// 		return Math.random();
-// 	})
-// };
+uniforms.iTime = -1;
+uniforms.iMouse = [0, 0];
+uniforms.iResolution = [window.innerWidth, window.innerHeight];
+uniforms.iChannel0 = new PIXI.Texture(new PIXI.BaseTexture(texture));
+
 export default class MenuBackgroundFilter extends PIXI.Filter {
 	constructor() {
-		super("", fragment, uniforms);
+		super(null, fragment, uniforms);
+		log.debug(this.uniforms);
 	}
 
 	get time() {
@@ -42,10 +23,12 @@ export default class MenuBackgroundFilter extends PIXI.Filter {
 	}
 
 	set time(val: number) {
+		// log.debug(this.uniforms.iTime.value, val);
 		this.uniforms.iTime = val;
 	}
 
 	set mouse(point: { x: number, y: number }) {
+		// log.debug(point, this.uniforms.iMouse);
 		this.uniforms.iMouse = [point.x, point.y];
 	}
 }
--- src/Filter/Test/fragment_f.glsl
+++ src/Filter/Test/fragment_f.glsl
@@ -1,5 +1,4 @@
-
-precision mediump float;
+precision highp float;
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
 uniform sampler2D iChannel0;
@@ -7,34 +6,36 @@ uniform sampler2D iChannel0;
 uniform vec2 iResolution;
 uniform vec2 iMouse;
 uniform float iTime;
-uniform vec4 filterArea;
+//uniform vec4 filterArea;
+uniform vec4 inputSize;
+uniform vec4 outputFrame;
 
-void main( )
+void main()
 {
-	vec2 pixelCoord = vTextureCoord * filterArea.xy;
-	vec2 offFromCenter = (pixelCoord - iResolution/2.)/iResolution;
+    vec2 pixelCoord = vTextureCoord * inputSize.xy + outputFrame.xy;
+    vec2 offFromCenter = (pixelCoord - iResolution/2.)/iResolution;
 
-	gl_FragColor = texture2D(iChannel0, vTextureCoord);
+    gl_FragColor = texture2D(iChannel0, vTextureCoord);
 
-	if(abs(pixelCoord.y - iMouse.y)<10. || abs(pixelCoord.x - iMouse.x)<10.){
-		gl_FragColor = vec4(sin(pixelCoord.x/100.), cos(pixelCoord.y/100.), abs(sin(iTime)), 0.5);
-	}
+    if (abs(pixelCoord.y - iMouse.y)<10. || abs(pixelCoord.x - iMouse.x)<10.){
+        gl_FragColor = vec4(sin(pixelCoord.x/100.), cos(pixelCoord.y/100.), abs(sin(iTime)), 0.5);
+    }
 
-	if(iResolution.x-pixelCoord.x < 10.){
-		gl_FragColor = vec4(1.,1.,0.,1.);
-	}
-	if(iResolution.y-pixelCoord.y < 10.){
-		gl_FragColor = vec4(1.,1.,0.,1.);
-	}
-	gl_FragColor += vec4(offFromCenter, 0., 0.);
-	// gl_FragColor = vec4(1.);
-	// for (int i = 0; i < 3; i++) {
-	// 	if(gl_FragCoord.x<seeds[i].x * iResolution.x){
-	// 		gl_FragColor *= 0.8;
-	// 	}
-	// 	if(gl_FragCoord.y<seeds[i].y * iResolution.y){
-	// 		gl_FragColor *= 0.8;
-	// 	}
-	// }
+    if (iResolution.x-pixelCoord.x < 10.){
+        gl_FragColor = vec4(1., 1., 0., 1.);
+    }
+    if (iResolution.y-pixelCoord.y < 10.){
+        gl_FragColor = vec4(1., 1., 0., 1.);
+    }
+    gl_FragColor += vec4(offFromCenter, 0., 0.);
+    // gl_FragColor = vec4(1.);
+    // for (int i = 0; i < 3; i++) {
+    // 	if(gl_FragCoord.x<seeds[i].x * iResolution.x){
+    // 		gl_FragColor *= 0.8;
+    // 	}
+    // 	if(gl_FragCoord.y<seeds[i].y * iResolution.y){
+    // 		gl_FragColor *= 0.8;
+    // 	}
+    // }
 
 }
